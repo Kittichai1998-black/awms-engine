ea96e9265310c22d69bde7c712290978cd1a5912
Anon <arebarozz@gmail.com>
2018-06-27 15:47:17 +0700

    ins upd 80%

--- AWMSEngine/ADO/InsUpdADO.cs
+++ AWMSEngine/ADO/InsUpdADO.cs
@@ -11,60 +11,63 @@ namespace AWMSEngine.ADO
 {
     public class InsUpdADO : BaseMSSQLAccess<InsUpdADO>
     {
-        public int Insert(string table_name, Dictionary<string,dynamic> recvlist, string con,
+        public int InsUpd(string table_name, List<Dictionary<string,dynamic>> recvlist, string con,
             AMWLogger logger, SqlTransaction trans = null)
         {
             Dictionary<string, dynamic> selectlist = new Dictionary<string, dynamic>();
-            var param = new Dapper.DynamicParameters();
-            foreach(var data in recvlist.SkipWhile(pk => pk.Key == con))
-            {
-                param.Add("@" + data.Key.ToString(), data.Value);
-                selectlist.Add(data.Key.ToString(), data.Value);
-            }
 
             string columns = "";
             string parameter = "";
-            foreach (var rowlist in selectlist)
+            string update = "";
+            string condition = "";
+            string insupd = "";
+
+            foreach (var row in recvlist)
+            {
+                foreach (var data in row)
+                {
+                    selectlist.Add(data.Key.ToString(), data.Value);
+                }
+                break;
+            }
+
+            foreach (var rowlist in selectlist.SkipWhile(pk => pk.Key == con))
             {
                 columns = columns + ", " + rowlist.Key;
                 parameter = parameter + ", @" + rowlist.Key;
-            }
-
-            string ins = string.Format("insert into {0} ({1}) values ({2})",
-                table_name, 
-                columns.Substring(2),
-                "@" + parameter.Substring(3));
-
-            var res = this.Execute(ins,
-                                CommandType.Text, param, logger, trans);
-            return res;
-        }
-
-        public int Update(string table_name, Dictionary<string, dynamic> updlist, string con,
-            AMWLogger logger, SqlTransaction trans = null)
-        {
-            var selectlist = new Dictionary<string, dynamic>();
-            var param = new Dapper.DynamicParameters();
-            foreach (var data in updlist.SkipWhile(pk => pk.Key == con))
-            {
-                param.Add("@" + data.Key.ToString(), data.Value);
-                selectlist.Add(data.Key.ToString(), data.Value);
-            }
-
-            string update = "";
-            string condition = string.Format(con + " = {0}", updlist[con]);
-            foreach (var rowlist in selectlist)
-            {
                 update = update + ", " + rowlist.Key + " = @" + rowlist.Key;
             }
 
-            string upd = string.Format("update {0} set {1} where {2}",
-                table_name,
-                update.Substring(2),
-                condition);
 
-            var res = this.Execute(upd,
-                                CommandType.Text, param, logger, trans);
+            int res = 0;
+            foreach (var row in recvlist)
+            {
+                condition = string.Format(con + " = {0}", row[con] ?? String.Empty);
+                var param = new Dapper.DynamicParameters();
+                foreach (var data in row.SkipWhile(pk => pk.Key == con))
+                {
+                    param.Add("@" + data.Key.ToString(), data.Value);
+                }
+
+                if (row[con] == null ?? "")
+                {
+                    insupd = string.Format("insert into {0} ({1}) values ({2})",
+                        table_name,
+                        columns.Substring(2),
+                        "@" + parameter.Substring(3));
+                }
+                else
+                {
+                    insupd = string.Format("update {0} set {1} where {2}",
+                        table_name,
+                        update.Substring(2),
+                        condition);
+                }
+
+                res = this.Execute(insupd,
+                                    CommandType.Text, param, logger, trans);
+                res += res;
+            }
             return res;
         }
     }
