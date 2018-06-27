19da255eb76bec48dfb4e8c4bbc5072afe46e1a2
apinya poosririt <sci_cs_bowling@hotmail.com>
2018-06-27 15:59:13 +0700

    no

--- AWMSEngine/Engine/General/InsertSql.cs
+++ AWMSEngine/Engine/General/InsertSql.cs
@@ -7,41 +7,53 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace AWMSEngine.Engine.General
 {
     public class InsertSql : BaseEngine
     {
-        public const string KEY_OUT_Status = "Status";
+        public const string KEY_IN_Ins = "Insert";
+        public const string KEY_IN_Con = "Condition";
+        public const string KEY_OUT_Result = "Status";
 
-        [EngineParamAttr(EngineParamAttr.InOutType.Response, KEY_OUT_Status, "Return Status")]
-        public RefVO<int> OutStatus { get; set; }
+        [EngineParamAttr(EngineParamAttr.InOutType.Request, KEY_IN_Ins, "Insert Data")]
+        public RefVO<Dictionary<string, dynamic>> InsertData { get; set; }
+        [EngineParamAttr(EngineParamAttr.InOutType.Request, KEY_IN_Con, "Condition Data")]
+        public RefVO<Dictionary<string, dynamic>> ConditionData { get; set; }
+
+        [EngineParamAttr(EngineParamAttr.InOutType.Response, KEY_OUT_Result, "Return Status")]
+        public RefVO<Dictionary<string,dynamic>> OutResult { get; set; }
 
         protected override void ExecuteEngine()
         {
+            var tokenModel = 0;
             var get_bu = this.BuVO.Get<dynamic>("_REQUEST");
-            var get_table = get_bu.t;
-            var get_ins = get_bu.datas;
-            var get_condition = get_bu.pk;
+            var get_table = get_bu.t.ToString();
+            var get_jins = JsonConvert.SerializeObject(get_bu.datas);
+            var get_condition = get_bu.pk.ToString();
+            var get_ins = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(get_jins);
 
-            if (get_condition == null)
+            if (get_ins[get_condition] == null ?? "")
             {
-                var tokenModel = ADO.InsUpd.GetInstant().Insert(
-                    get_table,
-                    get_ins,
-                    this.Logger);
-
-                this.OutStatus.Value = tokenModel;
-            }
-            else
-            {
-                var tokenModel = ADO.InsUpd.GetInstant().Update(
+                    tokenModel = ADO.InsUpdADO.GetInstant().Insert(
                     get_table,
                     get_ins,
                     get_condition,
                     this.Logger);
-
-                this.OutStatus.Value = tokenModel;
+            }
+            else
+            {
+                    tokenModel = ADO.InsUpdADO.GetInstant().Update(
+                    get_table,
+                    get_ins,
+                    get_condition,
+                    this.Logger);
+            }
+            if (tokenModel != 0)
+            {
+                this.OutResult.Value = get_ins;
             }
         }
     }
