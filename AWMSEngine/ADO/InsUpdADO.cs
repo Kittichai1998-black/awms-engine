50cc000cc2dee2ce09c63cf9d404549b7b864871
Anon <arebarozz@gmail.com>
2018-06-28 14:25:51 +0700

    ins upd 100%

--- AWMSEngine/ADO/InsUpdADO.cs
+++ AWMSEngine/ADO/InsUpdADO.cs
@@ -11,7 +11,7 @@ namespace AWMSEngine.ADO
 {
     public class InsUpdADO : BaseMSSQLAccess<InsUpdADO>
     {
-        public int InsUpd(string table_name, List<Dictionary<string,dynamic>> recvlist, string con,
+        public int InsUpd(string table_name, List<Dictionary<string,dynamic>> recvlist, string con, bool revision,
             AMWLogger logger, SqlTransaction trans = null)
         {
             Dictionary<string, dynamic> selectlist = new Dictionary<string, dynamic>();
@@ -38,7 +38,6 @@ namespace AWMSEngine.ADO
                 update = update + ", " + rowlist.Key + " = @" + rowlist.Key;
             }
 
-
             int res = 0;
             foreach (var row in recvlist)
             {
@@ -49,24 +48,44 @@ namespace AWMSEngine.ADO
                     param.Add("@" + data.Key.ToString(), data.Value);
                 }
 
-                if (row[con] == null ?? "")
+                if(revision == true)
                 {
+                    insupd = string.Format("update {0} set {1} , [Status] = 2 where {2}",
+                        table_name,
+                        update.Substring(2),
+                        condition);
+
+                    this.Execute(insupd,
+                                    CommandType.Text, param, logger, trans);
+
                     insupd = string.Format("insert into {0} ({1}) values ({2})",
                         table_name,
                         columns.Substring(2),
                         "@" + parameter.Substring(3));
+
+                    res = this.Execute(insupd,
+                                    CommandType.Text, param, logger, trans) + res;
                 }
                 else
                 {
-                    insupd = string.Format("update {0} set {1} where {2}",
-                        table_name,
-                        update.Substring(2),
-                        condition);
-                }
+                    if (row[con] == null ?? "")
+                    {
+                        insupd = string.Format("insert into {0} ({1}) values ({2})",
+                            table_name,
+                            columns.Substring(2),
+                            "@" + parameter.Substring(3));
+                    }
+                    else
+                    {
+                        insupd = string.Format("update {0} set {1} where {2}",
+                            table_name,
+                            update.Substring(2),
+                            condition);
+                    }
 
-                res = this.Execute(insupd,
-                                    CommandType.Text, param, logger, trans);
-                res += res;
+                    res = this.Execute(insupd,
+                                    CommandType.Text, param, logger, trans) + res;
+                }
             }
             return res;
         }
