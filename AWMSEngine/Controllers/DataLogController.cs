eb9bff6adf0072da935dbeaa3a22543bc2f4f9b5
Rattana Oudtagosum <glassbits.kk@gmail.com>
2018-08-27 09:49:37 +0700

    Add Get DataLog

---
+++ AWMSEngine/Controllers/DataLogController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AMWUtil.Common;
+using AWMSEngine.Engine.APIService.Mst;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWMSEngine.Controllers
+{
+    [Route("api/log")]
+    [ApiController]
+    public class DataLogController : ControllerBase
+    {
+
+        [HttpGet]
+        public dynamic GetData()
+        {
+            var jsond = ObjectUtil.QueryStringToObject(this.Request.QueryString.Value);
+            var api = new AWMSEngine.Engine.APIService.Data.SelectDataLogAPI();
+            var res = api.Execute(jsond);
+            return res;
+        }
+
+    }
+}
---
+++ AWMSEngine/Engine/APIService/Data/SelectDataLogAPI.cs
@@ -0,0 +1,32 @@
+﻿using AWMSModel.Constant.StringConst;
+using AWMSModel.Criteria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWMSEngine.Engine.APIService.Data
+{
+    public class SelectDataLogAPI : BaseAPIService
+    {
+        
+        protected override dynamic ExecuteEngineManual()
+        {
+            var res1= new General.SelectSql().Execute(this.Logger, this.BuVO,
+                new General.SelectSql.TReqModel()
+                {
+                    table_prefix = "aml_",
+                    t = this.RequestVO.t,
+                    ft = this.RequestVO.ft,
+                    f = this.RequestVO.f,
+                    g= this.RequestVO.g,
+                    l= this.RequestVO.l,
+                    q = this.RequestVO.q,
+                    s = this.RequestVO.s,
+                    sk = this.RequestVO.sk
+                });
+
+            return res1;
+        }
+    }
+}
