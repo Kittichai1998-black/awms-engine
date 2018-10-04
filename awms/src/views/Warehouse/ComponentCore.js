66868267a80cc3c0c07fbda7dcc24771ed5c6946
Anon <arebarozz@gmail.com>
2018-10-04 13:35:05 +0700

    core

--- awms/src/views/Warehouse/ComponentCore.js
+++ awms/src/views/Warehouse/ComponentCore.js
@@ -48,6 +48,36 @@ class AutoSelect extends Component{
     }
 }
 
+class NumberInput extends Component{
+    constructor(){
+        super()
+        this.state ={
+            value:1,
+        }
+    }
+    onKeyPress(event) {
+        const keyCode = event.keyCode || event.which;
+        const keyValue = String.fromCharCode(keyCode);
+        if (/\+|-/.test(keyValue) )
+          event.preventDefault();
+    }
+
+    render(){
+        return(
+            <Input onKeyPress={this.onKeyPress.bind(this)} onChange={e => {
+                if(e.target.value >= 0){
+                    this.props.onChange(e.target.value);
+                    this.setState({value:e.target.value})
+                }}
+            }
+            value={this.props.value}
+            type="number" 
+            step="1"
+            style={this.props.style}/>
+        )
+    }
+}
+
 const Clone = (obj) => {
     let copy;
   
@@ -82,4 +112,4 @@ const Clone = (obj) => {
     throw new Error("Unable to copy obj! Its type isn't supported.");
   }
 
-export {AutoSelect, Clone, TableGen, ExtendTable}
+export {AutoSelect, Clone, TableGen, ExtendTable, NumberInput}
