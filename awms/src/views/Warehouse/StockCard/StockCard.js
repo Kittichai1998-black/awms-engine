c72728a0f496be5c096258cf03e3d1578e03cf59
6100871 <6100871@BDF01-AMW618311>
2018-11-02 08:09:16 +0700

    commit

--- awms/src/views/Warehouse/StockCard/StockCard.js
+++ awms/src/views/Warehouse/StockCard/StockCard.js
@@ -55,7 +55,7 @@ class StockCard extends Component{
       sk:0,
       l:0,
       all:"",},
-      PackMasterdata:[]
+      PackMasterdata:[],
     
     }
     this.onCalculate = this.onCalculate.bind(this)
@@ -80,61 +80,71 @@ class StockCard extends Component{
   }
 
   onGetDocument(){
+    if(this.state.dateFrom===undefined ||this.state.dateTo === undefined || this.state.CodePack === undefined ) {
+      alert("Please select data")
+    } else {
     let formatDateFrom = this.state.dateFrom.format("YYYY-MM-DD")
     let formatDateTo = this.state.dateTo.format("YYYY-MM-DD")
-    let QueryDoc = this.state.selectdata
-    let JSONDoc = []
-    JSONDoc.push({"f": "DocumentDate", "c":"<=", "v":formatDateTo},
-      {"f": "Code", "c":"=", "v":this.state.CodePack},{"f": "Status", "c":"=", "v":"3"})
-    QueryDoc.q = JSON.stringify(JSONDoc)
-    Axios.get(createQueryString(QueryDoc)).then((res) => {    
-      this.setState({data:res.data.datas},()=>{
-        let dateDoc = this.state.data.filter(row=>{
-          return row.DocumentDate >= formatDateFrom
-        })
-        let dateThrow = this.state.data.filter(row2=>{
-          //console.log(row2.Quantity)
-          return row2.DocumentDate <= formatDateFrom  
-          
-        })
-        let sum = 0
-        var arrdata =[]
-        dateThrow.forEach(row=>{
-          sum+= row.Quantity         
-        })
-        arrdata.push({Total:sum})
-        //this.setState({data:dateDoc})
-        let sumDebit =0
-        let sumCredit=0
-        dateDoc.forEach(row=>{
-       
-         if(row.DocumentType_ID==='1001'){
-          sum=row.Quantity+sum
-          arrdata.push({DocumentType_ID:row.DocumentType_ID,DocumentDate:row.DocumentDate,DocCode:row.DocCode,Debit:row.Quantity,Total:sum})
-          sumDebit+=row.Quantity
-         }else if (row.DocumentType_ID==='1002'){
-          sum=row.Quantity-sum
-          arrdata.push({DocumentType_ID:row.DocumentType_ID,DocumentDate:row.DocumentDate,DocCode:row.DocCode,Credit:row.Quantity,Total:sum})
-          sumCredit+=row.Quantity
-         } else {
-           if (row.Quantity>=0){
-            sum=row.Quantity+sum
-            arrdata.push({DocumentType_ID:row.DocumentType_ID,DocumentDate:row.DocumentDate,DocCode:row.DocCode,Debit:row.Quantity,Total:sum})
-            sumDebit+=row.Quantity
-           } else {
-            sum=row.Quantity-sum
-            arrdata.push({DocumentType_ID:row.DocumentType_ID,DocumentDate:row.DocumentDate,DocCode:row.DocCode,Debit:row.Quantity,Total:sum})
-            sumCredit+=row.Quantity
-           }
-         }
+    
+      if (formatDateFrom > formatDateTo ){
+        alert("Choose the wrong information")
+      } else {
 
-        })
-        arrdata.push({Total:sum,Debit:sumDebit,Credit:sumCredit,DocumentDate:'Total'})
-        this.setState({data1:arrdata})
+        let QueryDoc = this.state.selectdata
+        let JSONDoc = []
+        JSONDoc.push({"f": "DocumentDate", "c":"<=", "v":formatDateTo},
+          {"f": "Code", "c":"=", "v":this.state.CodePack},{"f": "Status", "c":"=", "v":"3"})
+        QueryDoc.q = JSON.stringify(JSONDoc)
+            Axios.get(createQueryString(QueryDoc)).then((res) => { 
+            this.setState({data:res.data.datas},()=>{
+            //console.log(this.state.data)
+            if(this.state.data.length > 0){
+              let dateDoc = this.state.data.filter(row=>{
+              return row.DocumentDate >= formatDateFrom
+              })
+              let dateThrow = this.state.data.filter(row2=>{
+              return row2.DocumentDate <= formatDateFrom           
+              })
+              let sum = 0
+              var arrdata =[]
+                dateThrow.forEach(row=>{
+                  sum+= row.Quantity         
+                })
+            arrdata.push({DocumentType_ID:'Bring Forward',Total:sum})
 
-      })
-        
-    })
+            let sumDebit =0
+            let sumCredit=0
+            dateDoc.forEach(row=>{
+              if(row.DocumentType_ID===1001){
+                sum=row.Quantity+sum 
+                arrdata.push({DocumentType_ID:row.Name,DocumentDate:row.DocumentDate,DocCode:row.DocCode,Debit:row.Quantity,Total:sum})
+                sumDebit+=row.Quantity
+              } else if (row.DocumentType_ID===1002){
+                sum = Math.abs(row.Quantity-sum)
+                arrdata.push({DocumentType_ID:row.Name,DocumentDate:row.DocumentDate,DocCode:row.DocCode,Credit:Math.abs(row.Quantity),Total:sum})
+                sumCredit+=row.Quantity
+              } else {
+                if (row.Quantity>=0){              
+                  sum=row.Quantity+sum
+                  arrdata.push({DocumentType_ID:row.Name,DocumentDate:row.DocumentDate,DocCode:row.DocCode,Debit:row.Quantity,Total:sum})
+                  sumDebit+=row.Quantity
+                } else {
+                  sum=row.Quantity+(sum)
+                  arrdata.push({DocumentType_ID:row.Name,DocumentDate:row.DocumentDate,DocCode:row.DocCode,Credit:Math.abs(row.Quantity),Total:sum})
+                  sumCredit+=(row.Quantity*(-1))
+                }
+              }
+            })
+            arrdata.push({Total:sum,Debit:sumDebit,Credit:sumCredit,DocumentDate:'Total'})
+            this.setState({data1:arrdata})
+
+          }else{
+            this.setState({data1:[]})
+          }
+          })       
+        })
+      }
+    }
   }
 
   onCalculate(){
@@ -146,7 +156,7 @@ class StockCard extends Component{
     const cols = [
       {accessor: 'DocumentDate', Header: 'Date',editable:false,},
       {accessor: 'DocCode', Header: 'Document',editable:false,},
-      {accessor: 'DocumentType_ID', Header: 'Document Type',editable:false,},
+      {accessor: 'DocumentType_ID', Header: 'Title',editable:false,},
       {accessor: 'Debit', Header: 'Debit', editable:false,},
       {accessor: 'Credit', Header: 'Credit', editable:false,},
       {accessor: 'Total', Header: 'Total', editable:false},
@@ -172,14 +182,14 @@ class StockCard extends Component{
                 <label style={{width:"50px",textAlign:"center"}}>To</label>
                 <div style={{display:"inline-block"}}>
                   {this.state.pageID ? <span>{this.state.dateTo.format("DD-MM-YYYY")}</span> : this.dateTimePickerTo()}
-                </div>
+                </div>{' '}
                 <Button color="primary" id="off" onClick={() => {this.onGetDocument()}}>OK</Button>
             </div>          
           </Col>
         </Row>
         </div>
          <br></br>
-        <ReactTable NoDataComponent={()=> null} sortable={false} style={{background:"white"}} filterable={false} showPagination={false} minRows={2} columns={cols} data={this.state.data1} />
+        <ReactTable NoDataComponent={()=><div style={{textAlign:"center",height:"100px",color:"rgb(200,206,211)"}}>No row found</div>} sortable={false} style={{background:"white"}} filterable={false} showPagination={false} minRows={2} columns={cols} data={this.state.data1} />
       </div>
 
 
