3c7843666e3c38f89b52ac6df72fbd9af4b6160e
6100871 <6100871@BDF01-AMW618311>
2018-11-06 08:29:25 +0700

    commit

--- awms/src/routes.js
+++ awms/src/routes.js
@@ -182,7 +182,7 @@ const routes = [
   { path: '/wm/sto/loading', exact: true, name: 'Loading Checklist', component: LoadingChecklist },
   { path: '/doc/stc/manage', exact: true, name: 'Stock Correction Manage ', component: Stock },
   { path: '/doc/stc/list', exact: true, name: 'Stock Correction Document', component: Stockview },
-  { path: '/wm/stc/stccard', exact: true, name: 'Stock Card', component: StockCard },
+  { path: '/sys/sto/stccard', exact: true, name: 'Stock Card', component: StockCard },
   //{ path: '/doc/gr/manage', exact: true, name: 'Goods Receive Manage', component: GoodsReceiveManage },
   { path: '/doc/crt/manage', exact: true, name: 'CurrentInv', component: CurrentInv},
   { path: '/doc/gr/view', exact: true, name: 'StorageDetail', component: StorageDetail},
