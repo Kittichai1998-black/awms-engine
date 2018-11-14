6e617b63f15d3ca8bcaf343cd6b6d7b61af90bb4
Rattana Oudtagosum <glassbits.kk@gmail.com>
2018-11-14 17:36:06 +0700

    14-11-2018

---
+++ AWMSModel/Entity/ams_AreaMasterLine.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AWMSModel.Entity
+{
+    class ams_AreaMasterLine
+    {
+    }
+}
---
+++ AWMSModel/Entity/ams_AreaMasterType.cs
@@ -0,0 +1,12 @@
+﻿using AWMSModel.Constant.EnumConst;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AWMSModel.Entity
+{
+    public class ams_AreaMasterType : BaseEntitySTD
+    {
+        public AreaMasterGroupType groupType;
+    }
+}
