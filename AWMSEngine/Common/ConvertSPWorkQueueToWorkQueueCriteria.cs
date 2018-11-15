e3c1563725b1984ba9021bd46844fdc1d3e046ce
Rattana Oudtagosum <glassbits.kk@gmail.com>
2018-11-15 10:24:45 +0700

    15-11-18

---
+++ AWMSEngine/Common/ConvertSPWorkQueueToWorkQueueCriteria.cs
@@ -0,0 +1,21 @@
+﻿using AWMSModel.Criteria.SP.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWMSEngine.Common
+{
+    public class ConvertSPWorkQueueToWorkQueueCriteria
+    {
+
+        public SPOutQueueResponseCriteria ConvertToSPOutQueueResponseCriteria()
+        {
+            SPOutQueueResponseCriteria res = new SPOutQueueResponseCriteria()
+            {
+                //areaCode = ADO.StaticValue.StaticValueManager.GetInstant().AreaMasterLines.Find(x => x.ID == this.AreaMaster_ID).Code
+            };
+            return res;
+        }
+    }
+}
---
+++ AWMSModel/Constant/EnumConst/IOType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AWMSModel.Constant.EnumConst
+{
+    public enum IOType
+    {
+        INPUT = 0,
+        OUTPUT = 1
+    }
+}
