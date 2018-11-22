5e4f87e3655d93467102ae088088035fd830e708
Rattana Oudtagosum <glassbits.kk@gmail.com>
2018-11-22 09:01:48 +0700

    add ams_AreaRoute

---
+++ AWMSModel/Entity/ams_AreaRoute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AWMSModel.Entity
+{
+    public class ams_AreaRoute : BaseEntityID
+    {
+        public int Sou_AreaMaster_ID;
+        public int? Sou_AreaLocationMaster_ID;
+        public int Des_AreaMaster_ID;
+        public int? Des_AreaLocationMaster_ID;
+    }
+}
