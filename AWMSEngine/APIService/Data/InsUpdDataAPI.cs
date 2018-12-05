65e0326e8095cee375c517c5f7caaffddaa747e3
6100949 <6100949@DBF02-AMW618364>
2018-12-05 11:34:47 +0700

    Update Table setup: filter objecttype and grouptype

--- AWMSEngine/APIService/Data/InsUpdDataAPI.cs
+++ AWMSEngine/APIService/Data/InsUpdDataAPI.cs
@@ -29,6 +29,7 @@ namespace AWMSEngine.APIService.Data
                     pk = this.RequestVO.pk,
                     t = this.RequestVO.t
                 });
+            this.CommitTransaction();
             var stManager = ADO.StaticValue.StaticValueManager.GetInstant();
             Type stManagerType = stManager.GetType();
             foreach(var mt in stManagerType.GetMethods())
