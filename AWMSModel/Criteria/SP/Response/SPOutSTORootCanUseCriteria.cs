f1d70b44147ab32d0d5990efabca24f9855833f9
apinya poosririt <sci_cs_bowling@hotmail.com>
2018-12-25 11:23:50 +0700

    update spoutstoroot

--- AWMSModel/Criteria/SP/Response/SPOutSTORootCanUseCriteria.cs
+++ AWMSModel/Criteria/SP/Response/SPOutSTORootCanUseCriteria.cs
@@ -36,7 +36,6 @@ namespace AWMSModel.Criteria.SP.Response
         public DateTime? prodDate;
         public DateTime? expDate;
         public DateTime createTime;
-        public DateTime createTime;
 
         public string lot;
         public string batch;
