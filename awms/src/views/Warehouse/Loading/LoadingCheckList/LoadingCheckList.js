58087a59846be78a3a3622f9efea9917c4403cd7
Fhay_949 <6100949@DBF02-AMW618364>
2018-12-29 14:00:40 +0700

    update check Permission

--- awms/src/views/Warehouse/Loading/LoadingCheckList/LoadingCheckList.js
+++ awms/src/views/Warehouse/Loading/LoadingCheckList/LoadingCheckList.js
@@ -1,142 +1,148 @@
 import React, { Component } from 'react';
 import "react-table/react-table.css";
-import {Input, Button, Row, Col } from 'reactstrap';
+import { Input, Button, Row, Col } from 'reactstrap';
 import ReactTable from 'react-table';
 import Axios from 'axios';
 import moment from 'moment';
 import DatePicker from 'react-datepicker';
-import {AutoSelect, apicall, createQueryString} from '../../ComponentCore';
+import { AutoSelect, apicall, createQueryString } from '../../ComponentCore';
 import 'react-datepicker/dist/react-datepicker.css';
 import _ from 'lodash'
-import {GetPermission,Nodisplay} from '../../../ComponentCore/Permission';
+import { GetPermission, CheckWebPermission, CheckViewCreatePermission } from '../../../ComponentCore/Permission';
 
 const API = new apicall();
 
-class LoadingDocument extends Component{
+class LoadingDocument extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data:[],
-      inputstatus:true,
-      pageID:null,
-      addstatus:false,
-      adddisplay:"inline-block",
-      auto_transport:[],
-      readonly:false,
-      transportselect:{queryString:window.apipath + "/api/tes",
-        t:"Document",
-        q:'[{ "f": "Status", "c":"=", "v": 1}]',
-        f:"ID,Code, Name",
-        g:"",
-        s:"[{'f':'ID','od':'asc'}]",
-        sk:0,
-        all:"",},
+      data: [],
+      inputstatus: true,
+      pageID: null,
+      addstatus: false,
+      adddisplay: "inline-block",
+      auto_transport: [],
+      readonly: false,
+      transportselect: {
+        queryString: window.apipath + "/api/tes",
+        t: "Document",
+        q: '[{ "f": "Status", "c":"=", "v": 1}]',
+        f: "ID,Code, Name",
+        g: "",
+        s: "[{'f':'ID','od':'asc'}]",
+        sk: 0,
+        all: "",
+      },
     }
     this.onHandleScanConso = this.onHandleScanConso.bind(this)
     this.getTableData = this.getTableData.bind(this)
-    this.documentselect = {queryString:window.apipath + "/api/viw",
-      t:"Document",
-      q:'[{ "f": "DocumentType_ID", "c":"=", "v": 1012},{ "f": "EventStatus", "c":"=", "v": 11},{ "f": "Status", "c":"=", "v": 1}]',
-      f:"ID,concat(Code, ' : ', DesCustomerName) as Code,transport",
-      g:"",
-      s:"[{'f':'DesCustomerName','od':'asc'}]",
-      sk:0,
-      all:"",}
-      this.displayButtonByPermission = this.displayButtonByPermission.bind(this)
+    this.documentselect = {
+      queryString: window.apipath + "/api/viw",
+      t: "Document",
+      q: '[{ "f": "DocumentType_ID", "c":"=", "v": 1012},{ "f": "EventStatus", "c":"=", "v": 11},{ "f": "Status", "c":"=", "v": 1}]',
+      f: "ID,concat(Code, ' : ', DesCustomerName) as Code,transport",
+      g: "",
+      s: "[{'f':'DesCustomerName','od':'asc'}]",
+      sk: 0,
+      all: "",
+    }
+    this.displayButtonByPermission = this.displayButtonByPermission.bind(this)
   }
-  async componentWillMount(){
+  async componentWillMount() {
+    document.title = "Loading Checklist : AWMS";
     //permission
-    this.setState({showbutton:"none"})
-    let data = await GetPermission()
-    Nodisplay(data,33,this.props.history)
-    this.displayButtonByPermission(data)
+    this.setState({ showbutton: "none" })
+    let dataGetPer = await GetPermission()
+    CheckWebPermission("Loading", dataGetPer, this.props.history);
+    this.displayButtonByPermission(dataGetPer)
     //permission
   }
   //permission
-  displayButtonByPermission(perID){
-    this.setState({perID:perID})
-    let check = false
-    perID.forEach(row => {
-        if(row === 33){
-          check = true
-        }else if(row === 34){
-          check = false
-        }
-      })
-        if(check === true){  
-            var PerButtonInput = document.getElementById("per_button_input")
-            PerButtonInput.remove()     
-            var PerButtonScan = document.getElementById("per_button_scan")
-            PerButtonScan.remove()    
-            var PerButtonCode = document.getElementById("transportCode")
-            PerButtonCode.remove() 
-            var PerButtonTable = document.getElementById("per_button_table")
-            PerButtonTable.remove() 
+  //   33	TransLC_view
+  // 34	TransLC_create&modify
 
-        }else if(check === false){
-            this.setState({showbutton:"block"})
-        }else{
-            this.props.history.push("/404")
-        } 
+  displayButtonByPermission(dataGetPer) {
+    let check = true
+    if (CheckViewCreatePermission("TransLC_view", dataGetPer)) {
+      check = true //แสดงข้อมูล
+    }
+    if (CheckViewCreatePermission("TransLC_create&modify", dataGetPer)) {
+      check = false //แก้ไข
+    }
+    if (check === true) {
+      var PerButtonInput = document.getElementById("per_button_input")
+      PerButtonInput.remove()
+      var PerButtonScan = document.getElementById("per_button_scan")
+      PerButtonScan.remove()
+      var PerButtonCode = document.getElementById("transportCode")
+      PerButtonCode.remove()
+      var PerButtonTable = document.getElementById("per_button_table")
+      PerButtonTable.remove()
+
+    } else if (check === false) {
+      this.setState({ showbutton: "block" })
+    } else {
+      this.props.history.push("/404")
+    }
   }
   //permission
 
-  getTableData(){
-    if(this.state.transportvalue !== undefined){
+  getTableData() {
+    if (this.state.transportvalue !== undefined) {
       API.get(window.apipath + "/api/wm/loading/conso?docID=" + this.state.transportvalue).then(res => {
-        let groupdata = _.groupBy(res.data.datas, (e) => {return e.id})
+        let groupdata = _.groupBy(res.data.datas, (e) => { return e.id })
         let groupdisplay = []
-            let packname = []
-            for(let row in groupdata){
-              groupdata[row].forEach(grow => {
-                packname.forEach((prow, index) => {
-                  if(prow === grow.packName)
-                    packname.splice(index, 1)
-                })
-                packname.push(grow.packName)
-              })
-              let result = groupdata[row][0]
-              result.item = packname.join(",")
-              groupdisplay.push(groupdata[row][0])
-              packname = []
-            }
-        this.setState({data:groupdisplay})
+        let packname = []
+        for (let row in groupdata) {
+          groupdata[row].forEach(grow => {
+            packname.forEach((prow, index) => {
+              if (prow === grow.packName)
+                packname.splice(index, 1)
+            })
+            packname.push(grow.packName)
+          })
+          let result = groupdata[row][0]
+          result.item = packname.join(",")
+          groupdisplay.push(groupdata[row][0])
+          packname = []
+        }
+        this.setState({ data: groupdisplay })
       })
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     API.get(createQueryString(this.documentselect)).then(res => {
-      this.setState({auto_transport : res.data.datas}, () => {
+      this.setState({ auto_transport: res.data.datas }, () => {
         const auto_transport = []
         this.state.auto_transport.forEach(row => {
-          auto_transport.push({value:row.ID, label:row.Code, transportID:row.transport})
+          auto_transport.push({ value: row.ID, label: row.Code, transportID: row.transport })
         })
-        this.setState({auto_transport})
+        this.setState({ auto_transport })
       })
     })
     this.getTableData()
   }
 
-  onHandleScanConso(){
-    let data = {docID:this.state.transportvalue, scanCode:this.state.consoCode, _token:localStorage.getItem("Token")}
-    API.post(window.apipath + "/api/wm/loading/conso" ,data).then(res => {
+  onHandleScanConso() {
+    let data = { docID: this.state.transportvalue, scanCode: this.state.consoCode, _token: localStorage.getItem("Token") }
+    API.post(window.apipath + "/api/wm/loading/conso", data).then(res => {
       this.getTableData()
-      this.setState({consoCode:[]})
+      this.setState({ consoCode: [] })
     })
   }
 
-  render(){
+  render() {
     const cols = [
-      {accessor: 'code', Header: 'Code',editable:false,},
-      {accessor: 'item', Header: 'Item',editable:false,},
-      {accessor: 'issuedCode', Header: 'Issued Document',editable:false,},
-      {accessor: 'isLoaded', Header: 'Loaded',editable:false, Cell:e => <span>{e.value === true ? "Loaded" : "Wait"}</span>},
+      { accessor: 'code', Header: 'Code', editable: false, },
+      { accessor: 'item', Header: 'Item', editable: false, },
+      { accessor: 'issuedCode', Header: 'Issued Document', editable: false, },
+      { accessor: 'isLoaded', Header: 'Loaded', editable: false, Cell: e => <span>{e.value === true ? "Loaded" : "Wait"}</span> },
     ];
 
-    return(
+    return (
       <div>
-      {/*
+        {/*
         column = คอลัมที่ต้องการแสดง
         data = json ข้อมูลสำหรับ select ผ่าน url
         ddlfilter = json dropdown สำหรับทำ dropdown filter
@@ -145,25 +151,25 @@ class LoadingDocument extends Component{
     
       */}
         <Row>
-          <Col><label style={{paddingRight:"10px"}}>Loading Document : </label>
-          <div style={{display:"inline-block",width:"300px"}}><AutoSelect selectfirst={false} data={this.state.auto_transport} result={(e) => this.setState({"transportvalue":e.value, "transporttext":e.label, "TransportID":e.transportID}, () => {this.getTableData()})}/></div></Col>
+          <Col><label style={{ paddingRight: "10px" }}>Loading Document : </label>
+            <div style={{ display: "inline-block", width: "300px" }}><AutoSelect selectfirst={false} data={this.state.auto_transport} result={(e) => this.setState({ "transportvalue": e.value, "transporttext": e.label, "TransportID": e.transportID }, () => { this.getTableData() })} /></div></Col>
         </Row>
         <Row>
-          <Col><label style={{paddingRight:"10px"}}>Transport : </label><span>{this.state.TransportID}</span></Col>
+          <Col><label style={{ paddingRight: "10px" }}>Transport : </label><span>{this.state.TransportID}</span></Col>
         </Row>
         <Row>
-          <Col>    
-            <div style={{display:this.state.showbutton}}>
-              <label id="per_button_input" style={{paddingRight:"10px"}}>Barcode : </label><Input id="transportCode" style={{width:'200px', display:"inline-block"}} type="text" value={this.state.consoCode} 
-                  autoFocus
-                  onChange={(e) => {
-                    this.setState({consoCode:e.target.value})
-                  }} 
-                  onKeyPress={e => {
-                    if(e.key === "Enter"){
-                      this.onHandleScanConso()
-                    }
-                  }}/>
+          <Col>
+            <div style={{ display: this.state.showbutton }}>
+              <label id="per_button_input" style={{ paddingRight: "10px" }}>Barcode : </label><Input id="transportCode" style={{ width: '200px', display: "inline-block" }} type="text" value={this.state.consoCode}
+                autoFocus
+                onChange={(e) => {
+                  this.setState({ consoCode: e.target.value })
+                }}
+                onKeyPress={e => {
+                  if (e.key === "Enter") {
+                    this.onHandleScanConso()
+                  }
+                }} />
               <Button id="per_button_scan" onClick={this.onHandleScanConso} color="primary">Scan</Button>
             </div>
           </Col>
@@ -172,8 +178,8 @@ class LoadingDocument extends Component{
         <ReactTable columns={cols} minRows={5} data={this.state.data} sortable={false} style={{ background: 'white' }} filterable={false}
           showPagination={false} NoDataComponent={() => null} defaultPageSize={100000} />
 
-        </div>
- 
+      </div>
+
     )
   }
 }
--- awms/src/views/Warehouse/Stock/Stock.js
+++ awms/src/views/Warehouse/Stock/Stock.js
@@ -6,7 +6,7 @@ import { TableGen } from '../MasterData/TableSetup';
 import { apicall } from '../ComponentCore';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
-import {GetPermission,Nodisplay} from '../../ComponentCore/Permission';
+import { GetPermission, CheckWebPermission } from '../../ComponentCore/Permission';
 
 
 
@@ -25,15 +25,15 @@ const API = new apicall()
 const getMapSto = true
 
 
-class Stock extends Component{
+class Stock extends Component {
   constructor(props) {
     super(props);
     this.state = {
       getMapSto: true
-    
-   }
 
-    this.stockItem ={
+    }
+
+    this.stockItem = {
       queryString: window.apipath + "/api/trx",
       t: "Document",
       q: "[{ 'f': 'DocumentType_ID', c:' = ', 'v': 2003}]",
@@ -42,30 +42,30 @@ class Stock extends Component{
       s: "[{'f':'Code','od':'asc'}]",
       sk: 0,
       l: 20,
-      all: "",}
+      all: "",
+    }
 
-    
+
   }
-  async componentWillMount(){
+  async componentWillMount() {
+    document.title = "Stock Correction Manage : AWMS";
     //permission
-    this.setState({showbutton:"none"})
-    let data = await GetPermission()
-    Nodisplay(data,36,this.props.history)
-    
-    //permission
+    // 36	WarehouseSCD_view
+    // 37	WarehouseSCD_create&modify
+    this.setState({ showbutton: "none" })
+    let dataGetPer = await GetPermission()
+    CheckWebPermission("STCM", dataGetPer, this.props.history);
   }
- 
 
   componentDidMount() {
-
-    API.get(createQueryString(this.stockItem)).then(stocks => {})
+    API.get(createQueryString(this.stockItem)).then(stocks => { })
   }
 
-workingData(){
-    if(this.state.date){
+  workingData() {
+    if (this.state.date) {
       let postdata = {
-        "exportName":"DocumentAuditToCD",
-        "whereValues":[this.state.date]
+        "exportName": "DocumentAuditToCD",
+        "whereValues": [this.state.date]
       }
       API.post(window.apipath + "/api/report/export/fileServer", postdata)
     }
@@ -82,15 +82,15 @@ workingData(){
 
   render() {
     const cols = [
-      { accessor: 'Code', Header: 'Code', editable: false, filterable: false  },
+      { accessor: 'Code', Header: 'Code', editable: false, filterable: false },
       { accessor: 'DocumentDate', Header: 'Document Date', editable: false, filterable: false, Type: "datetime", dateformat: "datetime" },
-      { accessor: 'EventStatus', Header: 'Event Status', sortable: false, editable: false, filterable: false, Type: "EventStatus",  },
-      { accessor: 'Status', Header: 'Status', editable: false, filterable: false, Type: "DocumentStatus"  },
+      { accessor: 'EventStatus', Header: 'Event Status', sortable: false, editable: false, filterable: false, Type: "EventStatus", },
+      { accessor: 'Status', Header: 'Status', editable: false, filterable: false, Type: "DocumentStatus" },
       { accessor: 'CreateTime', Header: 'Create', editable: false, filterable: false, Type: "datetime", dateformat: "datetime" },
       { accessor: 'ModifyTime', Header: 'Modify', editable: false, Type: "datetime", dateformat: "datetime", filterable: false },
       { accessor: 'Remark', Header: 'Remark', editable: false, filterable: false },
       { Header: '', Aggregated: "button", Type: "button", filterable: false, sortable: false, btntype: "Detail", btntext: "Detail" },
-   
+
       /* {Header: '', Aggregated:"button",Type:"button", filterable:false, sortable:false, btntype:"Remove", btntext:"Remove"}, */
     ];
 
@@ -100,16 +100,16 @@ workingData(){
       func: this.createDetial
 
     }]
- 
-    return(
-      
+
+    return (
+
       <div>
         <Card>
-        </Card>   
-        <TableGen column={cols} 
-          data={this.stockItem}  
+        </Card>
+        <TableGen column={cols}
+          data={this.stockItem}
           filterable={true} uneditcolumn={this.uneditcolumn}
-          btn={btnfunc} 
+          btn={btnfunc}
           table="storck" />
       </div>
     )
