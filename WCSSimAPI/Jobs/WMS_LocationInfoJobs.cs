5209be52ef184516a21ef7f3534d8995d94e72d3
Rattana Oudtagosum <glassbits.kk@gmail.com>
2019-02-02 12:10:55 +0700

    02022019

---
+++ WCSSimAPI/Jobs/WMS_LocationInfoJobs.cs
@@ -0,0 +1,24 @@
+﻿using AMWUtil.Common;
+using AMWUtil.DataAccess.Http;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WCSSimAPI.Jobs
+{
+    public class WMS_LocationInfoJobs : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            var req = ADO.DataADO.GetInstant().list_request_wms_location_info(null);
+            if (!string.IsNullOrWhiteSpace(req.basecode))
+            {
+                var res = AMWUtil.DataAccess.Http.RESTFulAccess.SendJson<dynamic>(null, ConstConfig.WMSApiURL + "/api/wm/asrs/queue/working", RESTFulAccess.HttpMethod.POST, req.sJson.Json<dynamic>());
+                //ADO.DataADO.GetInstant().set_response_wms_location_info(null, req.basecode, ObjectUtil.Json(res));
+            }
+            return null;
+        }
+    }
+}
--- WCSSimAPI/Startup.cs
+++ WCSSimAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using WCSSimAPI.Jobs;
 
 namespace WCSSimAPI
 {
@@ -31,6 +32,11 @@ namespace WCSSimAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            AMWUtil.Common.SchedulerUtil.Start<WMS_RegisterJobs>(ConstConfig.CronEx);
+            AMWUtil.Common.SchedulerUtil.Start<WMS_WorkingJobs>(ConstConfig.CronEx);
+            AMWUtil.Common.SchedulerUtil.Start<WMS_DoneJobs>(ConstConfig.CronEx);
+            AMWUtil.Common.SchedulerUtil.Start<WMS_LocationInfoJobs>(ConstConfig.CronEx);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
