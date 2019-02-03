b1063f68989f6f2c2e927a0b8bd14bca8d673e92
Rattana Oudtagosum <glassbits.kk@gmail.com>
2019-02-03 14:33:00 +0700

    Reject GI

--- AWMSEngine/APIService/Doc/RejectedGIDocAPI.cs
+++ AWMSEngine/APIService/Doc/RejectedGIDocAPI.cs
@@ -18,7 +18,7 @@ namespace AWMSEngine.APIService.Doc
         protected override dynamic ExecuteEngineManual()
         {
             this.BeginTransaction();
-            var req = ObjectUtil.DynamicToModel<RejectedGIDocumnet.TDocReq>(this.RequestVO);
+            var req = ObjectUtil.DynamicToModel<RejectedGIDocumnet.TReq>(this.RequestVO);
             var res = new RejectedGIDocumnet().Execute(this.Logger, this.BuVO, req);
 
             return res;
--- AWMSEngine/Engine/Business/Issued/RejectedGIDocumnet.cs
+++ AWMSEngine/Engine/Business/Issued/RejectedGIDocumnet.cs
@@ -11,102 +11,33 @@ using System.Threading.Tasks;
 
 namespace AWMSEngine.Engine.Business.Issued
 {
-    public class RejectedGIDocumnet : BaseEngine<RejectedGIDocumnet.TDocReq, RejectedGIDocumnet.TDocRes>
+    public class RejectedGIDocumnet : BaseEngine<RejectedGIDocumnet.TReq, RejectedGIDocumnet.TRes>
     {
 
-        public class TDocReq
+        public class TReq
         {
-            public long[] docIDs;
+            public List<long> docIDs;
         }
-        public class TDocRes
+        public class TRes
         {
             public List<amt_Document> documents;
         }
 
-        protected override TDocRes ExecuteEngine(TDocReq reqVO)
+        protected override TRes ExecuteEngine(TReq reqVO)
         {
-            TDocRes res = new TDocRes();
-            res.documents = new List<amt_Document>();
-            foreach(long id in reqVO.docIDs)
+
+            var docIssues = ADO.DocumentADO.GetInstant().ListAndRelationSupper(reqVO.docIDs, this.BuVO);
+            var docNotCloseds = docIssues.Where(x => x.EventStatus != DocumentEventStatus.IDLE);
+            if (docNotCloseds.Count() > 0)
+                throw new AMWException(this.Logger, AMWExceptionCode.V1001, "เอกสารรับเข้า '" + (string.Join(',', docNotCloseds.Select(x => x.Code).ToArray())) + "' ต้องมีสถานะ IDLE เท่านั้น");
+            
+            docIssues.ForEach(doc =>
             {
-                amt_Document doc = ADO.DataADO.GetInstant().SelectByID<amt_Document>(id, this.BuVO);
-
-                if (doc == null || doc.Status == EntityStatus.REMOVE)
-                    throw new AMWException(this.Logger, AMWExceptionCode.V1001, "DocumnetID " + id);
-
-             
-                if (doc.DocumentType_ID == DocumentTypeID.LOADING && doc.EventStatus == DocumentEventStatus.WORKED) {
-
-                    throw new AMWException(this.Logger, AMWExceptionCode.V1002, "สินค้าขึ้นรถแล้ว ไม่สามารถลบได้");
-                }
-
-
-             
-
-                //if (doc.Status == EntityStatus.DONE)
-                //    throw new AMWException(this.Logger, AMWExceptionCode.V1002, "Documnet is Done");
-
-                if (doc.Status == EntityStatus.ACTIVE && doc.EventStatus != DocumentEventStatus.IDLE && doc.EventStatus != DocumentEventStatus.WORKING)
-                {
-                    var stos = ADO.DocumentADO.GetInstant().ListStoInDocs(doc.ID.Value, this.BuVO);
-                    if (stos.Count > 0)
-                        throw new AMWException(this.Logger, AMWExceptionCode.V1002, "Documnet is " + doc.EventStatus);
-                }
-
-                
-
-                List<amt_DocumentItem> linkDocItems = ADO.DataADO.GetInstant().SelectBy<amt_DocumentItem>(
-                                                        new SQLConditionCriteria[] {
-                                                            new SQLConditionCriteria("LinkDocument_ID",doc.ID, SQLOperatorType.EQUALS),
-                                                            new SQLConditionCriteria("Status",2, SQLOperatorType.NOTEQUALS)
-                                                        }, this.BuVO);
-                if (linkDocItems.Count > 0)
-                {
-                    string[] docConfixs = ADO.DataADO.GetInstant().SelectBy<amt_Document>(
-                        new SQLConditionCriteria[] {
-                            new SQLConditionCriteria("ID",string.Join(",",linkDocItems.Select(x=>x.Document_ID).ToArray()), SQLOperatorType.IN)
-                        }, this.BuVO).Select(x=>x.Code).ToArray();
-                    throw new AMWException(this.Logger, AMWExceptionCode.V1002, "กรุณา Reject เอกสาร '" + string.Join(',', docConfixs) + "' ก่อน");
-
-                }
-
- 
-                var stoToReceives = ADO.DocumentADO.GetInstant().ListStoInDocs(doc.ID.Value, this.BuVO);
-                var stoLasters = ADO.DataADO.GetInstant().SelectBy<amt_StorageObject>(
-                        new SQLConditionCriteria[] {
-                            new SQLConditionCriteria("ID",string.Join(",",stoToReceives.Select(x=>x.StorageObject_ID).ToArray()), SQLOperatorType.IN)
-                        }, this.BuVO);
-                var rootStoToReceives = stoLasters.Where(x => x.Status == EntityStatus.ACTIVE || x.Status == EntityStatus.DONE)
-                    .GroupBy(x => x.ParentStorageObject_ID ?? x.ID)
-                    .Select(x => x.Key.Value);
-                
-                rootStoToReceives.ToList().ForEach(x =>
-                {
-                    ADO.StorageObjectADO.GetInstant().UpdateStatusToChild(x, null, EntityStatus.ACTIVE, StorageObjectEventStatus.RECEIVED, this.BuVO);
-                    ADO.StorageObjectADO.GetInstant().UpdateStatusToChild(x, null, EntityStatus.DONE, StorageObjectEventStatus.RECEIVED, this.BuVO);
-                });
-
-                ADO.DocumentADO.GetInstant().UpdateStatusToChild(id,
-                    DocumentEventStatus.IDLE, null,
-                    DocumentEventStatus.REJECTED,
-                    this.BuVO);
-
-                ADO.DocumentADO.GetInstant().UpdateStatusToChild(id,
-                    null, EntityStatus.ACTIVE,
-                    DocumentEventStatus.REJECTED, 
-                    this.BuVO);
-
-                ADO.DocumentADO.GetInstant().UpdateStatusToChild(id,
-                   null, EntityStatus.DONE,
-                   DocumentEventStatus.REJECTED,
-                   this.BuVO);
-
                 doc.EventStatus = DocumentEventStatus.REJECTED;
-                doc.Status = EntityStatus.REMOVE;
-                res.documents.Add(doc);
-            }
-
-            return res;
+                doc.Status = ADO.DocumentADO.GetInstant().UpdateStatusToChild(doc.ID.Value, null, EntityStatus.ACTIVE, DocumentEventStatus.REJECTED, this.BuVO);
+                
+            });
+            return new TRes { documents= docIssues };
         }
         
     }
