719929d100ec370c28a2e03639dda80d23c6008c
Ple <6100871@BDF01-AMW618311>
2019-02-06 11:32:01 +0700

    commit

---
+++ AWMSEngine/APIService/API2/CheckAPI.cs
@@ -0,0 +1,26 @@
+﻿using AMWUtil.Common;
+using AWMSEngine.Engine.Business;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWMSEngine.APIService.API2
+{
+    public class CheckAPI : BaseAPIService
+    {
+        public CheckAPI(ControllerBase controllerAPI) : base(controllerAPI)
+        {
+        }
+
+        protected override dynamic ExecuteEngineManual()
+        {
+            this.BeginTransaction();
+            var req = ObjectUtil.DynamicToModel<CheckSendToSAP.TReq>(this.RequestVO);
+            var res = new CheckSendToSAP().Execute(this.Logger, this.BuVO, req);
+            return res;
+
+        }
+    }
+}
