1349151687e3e08b44150f7adf683e857fad7a20
Fhay_949 <6100949@DBF02-AMW618364>
2019-02-09 13:37:10 +0700

    edit DocumentEventStatus

--- awms/src/views/Warehouse/Status.js
+++ awms/src/views/Warehouse/Status.js
@@ -45,9 +45,9 @@ const DocumentEventStatus = [
     {status:'IDLE' , code:10,},
     {status:'WORKING' , code:11,},
     {status:'WORKED' , code:12,},
-    {status:'REMOVING' , code:21,},
-    {status:'REMOVED' , code:22,},
-    {status:'REJECTING' , code:23,},
+    // {status:'REMOVING' , code:21,},
+    // {status:'REMOVED' , code:22,},
+    // {status:'REJECTING' , code:23,},
     {status:'REJECTED' , code:24,},
     {status:'CLOSING' , code:31,},
     {status:'CLOSED' , code:32,}
