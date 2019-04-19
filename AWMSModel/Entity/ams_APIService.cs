5f49d0eb7db17301b8a54a425feeafc79e17eabe
Anon <arebarozz@gmail.com>
2019-04-19 10:39:10 +0700

    add v2 Controller

--- AWMSModel/Entity/ams_APIService.cs
+++ AWMSModel/Entity/ams_APIService.cs
@@ -6,5 +6,10 @@ namespace AWMSModel.Entity
 {
     public class ams_APIService : BaseEntitySTD
     {
+        public int Permission_ID;
+        public int APIServiceGroup_ID;
+        public string FullClassName;
+        public string ActionCommand;
+        public int Revision;
     }
 }
