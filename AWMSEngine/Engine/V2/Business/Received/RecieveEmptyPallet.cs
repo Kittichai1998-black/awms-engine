db7513b5014816bf6bef00126d91823700ab08eb
Anon <arebarozz@gmail.com>
2019-04-26 09:17:28 +0700

    รับพาเลทเปล่า

--- AWMSEngine/Engine/V2/Business/Received/RecieveEmptyPallet.cs
+++ AWMSEngine/Engine/V2/Business/Received/RecieveEmptyPallet.cs
@@ -1,4 +1,5 @@
 ﻿using AMWUtil.Exception;
+using AWMSModel.Constant.EnumConst;
 using AWMSModel.Criteria;
 using AWMSModel.Entity;
 using System;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace AWMSEngine.Engine.V2.Business.Received
 {
-    public class RecieveEmptyPallet : BaseEngine<RecieveEmptyPallet.TReq, RecieveEmptyPallet.TRes>
+    public class MapEmptyPallet : BaseEngine<MapEmptyPallet.TReq, MapEmptyPallet.TRes>
     {
         public class TReq
         {
@@ -27,29 +28,49 @@ namespace AWMSEngine.Engine.V2.Business.Received
             var result = new TRes();
             var warehouse = this.StaticValue.Warehouses.FirstOrDefault(x => x.Code == reqVO.warehouseCode);
             if (warehouse == null)
-                throw new AMWException(this.Logger, AMWExceptionCode.V1001, "ไม่พบ Area นี้ในระบบ");
+                throw new AMWException(this.Logger, AMWExceptionCode.V1001, "ไม่พบ Warehouse นี้ในระบบ");
             var area = this.StaticValue.AreaMasters.FirstOrDefault(x => x.Code == reqVO.areaCode);
             if(area == null)
                 throw new AMWException(this.Logger, AMWExceptionCode.V1001, "ไม่พบ Area นี้ในระบบ");
 
-            var location = ADO.DataADO.GetInstant().SelectByCodeActive<ams_AreaLocationMaster>(reqVO.locationCode, this.BuVO).ID;
+            var location = ADO.DataADO.GetInstant().SelectByCodeActive<ams_AreaLocationMaster>(reqVO.locationCode, this.BuVO);
             if (location == null)
                 throw new AMWException(this.Logger, AMWExceptionCode.V1001, "ไม่พบ Location นี้ในระบบ");
 
-            var sto = ADO.DataADO.GetInstant().SelectBy<amt_StorageObject>(new SQLConditionCriteria[]{
-                new SQLConditionCriteria("AreaLocationMaster_ID", location, AWMSModel.Constant.EnumConst.SQLOperatorType.EQUALS),
-                new SQLConditionCriteria("Status", 1, AWMSModel.Constant.EnumConst.SQLOperatorType.EQUALS)
+            var stoLoc = ADO.DataADO.GetInstant().SelectBy<amt_StorageObject>(new SQLConditionCriteria[]{
+                new SQLConditionCriteria("AreaLocationMaster_ID", location.ID, SQLOperatorType.EQUALS),
+                new SQLConditionCriteria("Status", 1, SQLOperatorType.EQUALS)
             }, this.BuVO);
-            if(sto.Count > 0)
+            var stoPallet = ADO.DataADO.GetInstant().SelectBy<amt_StorageObject>(new SQLConditionCriteria[]{
+                new SQLConditionCriteria("Code", reqVO.baseCode, SQLOperatorType.EQUALS),
+                new SQLConditionCriteria("Status", 1, SQLOperatorType.EQUALS)
+            }, this.BuVO);
+            
+            if (stoPallet.Count > 0)
             {
-                result.recievedStatus = false;
+                throw new AMWException(this.Logger, AMWExceptionCode.V1002, "มี Pallet นี้อยู่ในระบบ ไม่สามารถกำกับได้");
+            }
+            if (stoLoc.Count > 0)
+            {
+                throw new AMWException(this.Logger, AMWExceptionCode.V1002, "Location มี Pallet อยู่แล้ว ไม่สามารถกำกับได้");
             }
             var createEmptyPalletSTO = new ScanMapStoNoDoc();
 
-            //var createData = ScanMapStoNoDoc
+            var createPalletData = new ScanMapStoNoDoc.TReq()
+            {
+                rootID = null,
+                scanCode = reqVO.baseCode,
+                warehouseID = warehouse.ID,
+                areaID = area.ID,
+                mode = VirtualMapSTOModeType.REGISTER,
+                action = VirtualMapSTOActionType.ADD,
+                amount = 1,
+                locationCode=location.Code,
+                batch = "",isRoot = true,lot = "",options = "",orderNo = "",productDate = null,unitCode = ""
+            };
 
-            var res = createEmptyPalletSTO.Execute(this.Logger, this.BuVO, null);
-
+            var res = createEmptyPalletSTO.Execute(this.Logger, this.BuVO, createPalletData);
+            result.recievedStatus = true;
 
             return result;
         }
