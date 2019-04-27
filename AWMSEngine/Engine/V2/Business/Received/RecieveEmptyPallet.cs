d37c4a75593ec4fada4fc89a49fe7ab5434de5c5
Anon <arebarozz@gmail.com>
2019-04-27 16:31:39 +0700

    fix again

--- AWMSEngine/Engine/V2/Business/Received/RecieveEmptyPallet.cs
+++ AWMSEngine/Engine/V2/Business/Received/RecieveEmptyPallet.cs
@@ -39,7 +39,7 @@ namespace AWMSEngine.Engine.V2.Business.Received
 
             var stoLoc = ADO.DataADO.GetInstant().SelectBy<amt_StorageObject>(new SQLConditionCriteria[]{
                 new SQLConditionCriteria("AreaLocationMaster_ID", location.ID, SQLOperatorType.EQUALS),
-                new SQLConditionCriteria("Status", 0, SQLOperatorType.EQUALS)
+                new SQLConditionCriteria("Status", new int[]{ 0,1 }, SQLOperatorType.IN)
             }, this.BuVO);
             var stoPallet = ADO.DataADO.GetInstant().SelectBy<amt_StorageObject>(new SQLConditionCriteria[]{
                 new SQLConditionCriteria("Code", reqVO.baseCode, SQLOperatorType.EQUALS),
