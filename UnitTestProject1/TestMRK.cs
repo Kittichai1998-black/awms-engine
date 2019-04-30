2d183e314c92c7b7ec14a8ef9e0679be211781e5
Anon <arebarozz@gmail.com>
2019-04-30 11:07:16 +0700

    add mrk proj

---
+++ UnitTestProject1/TestMRK.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestMRK
+    {
+        [TestMethod]
+        public void TestRead()
+        {
+            var proj = new ProjectMRK.Engine.Bussiness.ReadFileXML();
+            proj.test();
+        }
+    }
+}
