f971e376f4e3b29765b39c44cf3adb29f149cba6
sumonrut <sumonrut@kkumail.com>
2019-05-10 16:47:11 +0700

    cm

--- AWMSEngine/Engine/V2/Business/WorkQueue/BaseRegisterWorkQueue.cs
+++ AWMSEngine/Engine/V2/Business/WorkQueue/BaseRegisterWorkQueue.cs
@@ -57,7 +57,7 @@ namespace AWMSEngine.Engine.V2.Business.WorkQueue
         private SPOutAreaLineCriteria GetDesLocations(StorageObjectCriteria sto, List<amt_DocumentItem> docItems, TReq reqVO)
         {
             //reqVO.locationCode
-            var desLocations = ADO.AreaADO.GetInstant().ListDestinationArea(IOType.INPUT,sto.areaID,sto.parentID, this.BuVO);
+            var desLocations = ADO.AreaADO.GetInstant().ListDestinationArea(IOType.INPUT,sto.areaID.Value,sto.parentID, this.BuVO);
             foreach(var des in desLocations)
             {
                 if(!string.IsNullOrWhiteSpace(des.Condition_Eval) && Z.Expressions.Eval.Execute<bool>(des.Condition_Eval))
