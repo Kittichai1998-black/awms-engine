7dbb9e53262ba81dcab581299e3e0bedb855345b
Anon <arebarozz@gmail.com>
2019-05-14 13:16:13 +0700

    work queue

---
+++ ProjectMRK/Engine/Bussiness/WorkQueue/FastMoveQueue.cs
@@ -0,0 +1,31 @@
+﻿using AWMSEngine.Common;
+using AWMSEngine.Engine;
+using AWMSModel.Criteria;
+using AWMSModel.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectMRK.Engine.Bussiness.WorkQueue
+{
+    public class FastMoveQueue: IEval
+    {
+        public bool exec(dynamic data)
+        {
+            //var str = "FeatureExecute.EvalExec(\"ProjectMRK.Engine.Bussiness.WorkQueue.FastMoveQueue\", new { baseCode = reqVO.baseCode, buVO = this.BuVO });";
+            List<amt_StorageObject> stoIDs = AWMSEngine.ADO.StorageObjectADO.GetInstant().ListPallet(data.baseCode.ToString(), (VOCriteria)data.buVO).ToList();
+            List<amt_Document> docs = AWMSEngine.ADO.DocumentADO.GetInstant().ListBySTO(stoIDs.Select(x=>x.ID.Value).ToList(), (VOCriteria)data.buVO);
+            var fastMove = docs.Any(x => x.MovementType_ID == AWMSModel.Constant.EnumConst.MovementType.FG_FASTMOVE);
+            return fastMove;
+        }
+        private void CreateGIDocument(List<amt_Document> docs, List<amt_StorageObject> sto)
+        {
+            var createDocument = new AWMSEngine.Engine.Business.Issued.CreateDocument();
+            var doc = new AWMSEngine.Engine.Business.Issued.CreateDocument.TReq
+            {
+
+            };
+        }
+    }
+}
