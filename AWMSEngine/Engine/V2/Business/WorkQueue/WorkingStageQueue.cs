98eb0778a7ad5eec2ada5e80177a975aca2b1fb6
Fhay_949 <6100949@DBF02-AMW618364>
2019-05-17 10:36:17 +0700

    WorkingStageQueue

--- AWMSEngine/Engine/V2/Business/WorkQueue/WorkingStageQueue.cs
+++ AWMSEngine/Engine/V2/Business/WorkQueue/WorkingStageQueue.cs
@@ -27,7 +27,7 @@ namespace AWMSEngine.Engine.V2.Business.WorkQueue
             this.initMasterData(reqVO);
             var queueTrx = this.UpdateWorkQueueWork(reqVO);
             if (queueTrx.StorageObject_Code != reqVO.baseCode)
-                throw new AMWException(this.Logger, AMWExceptionCode.V1001, "Base Code '" + reqVO.baseCode + "' ไม่ตรงกับที่มีใน Work Queue '" + queueTrx.StorageObject_Code + "'");
+                throw new AMWException(this.Logger, AMWExceptionCode.V1001, "Base Code '" + reqVO.baseCode + "' doesn't match with in Work Queue '" + queueTrx.StorageObject_Code + "'");
 
             var baseInfo = this.UpdateStorageObject(reqVO, queueTrx);
 
