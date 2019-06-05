c028cc487a6582420ae4edc128aa15f2116fc464
Rattana Oudtagosum <glassbits.kk@gmail.com>
2019-06-05 15:13:34 +0700

    Add Project SAP Connect

---
+++ ProjectSTA_SAPConnect/Areas/HelpPage/ModelDescriptions/ComplexTypeModelDescription.cs
@@ -0,0 +1,14 @@
+using System.Collections.ObjectModel;
+
+namespace ProjectSTA_SAPConnect.Areas.HelpPage.ModelDescriptions
+{
+    public class ComplexTypeModelDescription : ModelDescription
+    {
+        public ComplexTypeModelDescription()
+        {
+            Properties = new Collection<ParameterDescription>();
+        }
+
+        public Collection<ParameterDescription> Properties { get; private set; }
+    }
+}
---
+++ ProjectSTA_SAPConnect/Controllers/SAPConnectController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProjectSTA_SAPConnect.Controllers
+{
+    public class SAPConnectController : ApiController
+    {
+        public dynamic Post([FromBody]Models.RequestCriteria value)
+        {
+            ADO.SAPExec exec = new ADO.SAPExec();
+            var res = exec.exec(value);
+            return res;
+        }
+    }
+}
