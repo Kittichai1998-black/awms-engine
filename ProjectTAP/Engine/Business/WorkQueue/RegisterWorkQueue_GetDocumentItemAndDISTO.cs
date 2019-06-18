9f7e924487f378cec2cc4d96496030e07372c863
Ple <6100871@BDF01-AMW618311>
2019-06-18 13:27:18 +0700

    commit

--- ProjectTAP/Engine/Business/WorkQueue/RegisterWorkQueue_GetDocumentItemAndDISTO.cs
+++ ProjectTAP/Engine/Business/WorkQueue/RegisterWorkQueue_GetDocumentItemAndDISTO.cs
@@ -2,6 +2,7 @@
 using AMWUtil.Logger;
 using AWMSEngine.ADO.StaticValue;
 using AWMSEngine.Engine;
+using AWMSEngine.Engine.Business.Received;
 using AWMSEngine.Engine.V2.Business.WorkQueue;
 using AWMSModel.Constant.EnumConst;
 using AWMSModel.Criteria;
@@ -11,7 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ProjectTAP.Engine.Business.Received
+namespace ProjectTAP.Engine.Business.WorkQueue
 {
     public class RegisterWorkQueue_GetDocumentItemAndDISTO : IProjectEngine<
         RegisterWorkQueue.TReqDocumentItemAndDISTO,
@@ -20,6 +21,77 @@ namespace ProjectTAP.Engine.Business.Received
     {
         public List<amt_DocumentItem> ExecuteEngine(AMWLogger logger, VOCriteria buVO, RegisterWorkQueue.TReqDocumentItemAndDISTO data)
         {
+            //var mapsto = data.sto;
+            //var StaticValue = AWMSEngine.ADO.StaticValue.StaticValueManager.GetInstant();
+            //MovementType FG_Movement = MovementType.FG_TRANSFER_WM;
+            //amt_Document doc = AWMSEngine.ADO.DocumentADO.GetInstant().ListDocs(DocumentTypeID.GOODS_RECEIVED, null, null, null, FG_Movement, buVO)
+            //                       //var doc = AWMSEngine.ADO.DocumentADO.GetInstant().ListDocs(DocumentTypeID.GOODS_RECEIVED, souBranchID, _warehouseASRS.ID, null, FG_Movement, this.BuVO)
+            //                       .FirstOrDefault(x => x.EventStatus == DocumentEventStatus.WORKING || x.EventStatus == DocumentEventStatus.NEW); //_areaASRS.ID
+            //                                                                                                                                       //Pack Info ไม่พบ Document Item ใดๆที่ตรงกับในระบบ
+            //if (doc == null)
+            //{
+            //    //สร้างเอกสารรับเข้า ปกติ FG_Transfer, FG_RETURNCUSTOMER
+            //    doc = new CreateGRDocument().Execute(logger, buVO,
+            //               new CreateGRDocument.TReq()
+            //               {
+            //                   refID = null,
+            //                   ref1 = null,
+            //                   ref2 = null,
+            //                   souBranchID = null,
+            //                   souWarehouseID = null,
+            //                   souAreaMasterID = null,
+            //                   desBranchID = StaticValue.Warehouses.First(x => x.ID == mapsto.warehouseID).Branch_ID,
+            //                   desWarehouseID = mapsto.warehouseID,
+            //                   desAreaMasterID = null,
+            //                   movementTypeID = FG_Movement,
+            //                   lot = null,
+            //                   batch = null,
+            //                   documentDate = DateTime.Now,
+            //                   actionTime = DateTime.Now,
+            //                   eventStatus = DocumentEventStatus.NEW,
+            //                   receiveItems = new List<CreateGRDocument.TReq.ReceiveItem>() {
+            //                                            new CreateGRDocument.TReq.ReceiveItem
+            //                                            {
+            //                                                packCode = packH.code,
+            //                                                quantity = null,
+            //                                                unitType = packH.unitCode,
+            //                                                batch = null,
+            //                                                lot = null,
+            //                                                orderNo = FG_Movement == MovementType.EPL_TRANSFER_WM ? null : packH.orderNo,
+            //                                                ref2 = null,
+            //                                                eventStatus = DocumentEventStatus.NEW,
+            //                                                docItemStos = new List<amt_DocumentItemStorageObject>() { ConverterModel.ToDocumentItemStorageObject(packH, null, null, null)}
+
+            //                                            }}
+            //               });
+
+            //    docItems.AddRange(doc.DocumentItems);
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             var reqVO = data.reqVO;
             var sto = data.sto;
             List<amt_DocumentItem> docItem = new List<amt_DocumentItem>();
