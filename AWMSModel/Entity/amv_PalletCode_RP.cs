e89129690e4af8bff3658230d6f49290b8301f47
Anon <arebarozz@gmail.com>
2019-06-19 13:55:29 +0700

    register queue

---
+++ AWMSModel/Entity/amv_PalletCode_RP.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AWMSModel.Entity
+{
+    public class amv_PalletCode_RP : IEntityModel
+    {
+        public long BaseID;
+        public string BaseCode;
+        public long PackID;
+        public string PackCode;
+        public string PackName;
+        public string Batch;
+        public long PackMasterID;
+        public string UnitCode;
+        public decimal Quantity;
+    }
+}
