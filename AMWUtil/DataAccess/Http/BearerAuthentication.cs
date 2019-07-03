7e92911827d3d0852b8d796e9ea125c333d0edf7
Woramet <dexter_top@hotmail.com>
2019-07-03 16:52:02 +0700

    LineNotify

---
+++ AMWUtil/DataAccess/Http/BearerAuthentication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AMWUtil.DataAccess.Http
+{
+   public  class BearerAuthentication : IAuthentication
+    {
+        public string Token { get; set; }
+        public BearerAuthentication(string token)
+        {
+            this.Token = token;
+        }
+    }
+}
---
+++ AMWUtil/DataAccess/Http/LineAccess.cs
@@ -0,0 +1,19 @@
+﻿using AMWUtil.Logger;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AMWUtil.DataAccess.Http
+{
+    public static class LineAccess
+    {
+        public static string Notify(AMWLogger logger, string token, string msg,  int retry = 0, int timeout = 30000)
+        {
+            var res = RESTFulAccess.SendJson<dynamic>(logger, "https://notify-api.line.me/api/notify", RESTFulAccess.HttpMethod.POST,
+                new { message = msg },
+                new BearerAuthentication(token), retry, timeout);
+
+            return res;
+        }
+    }
+}
