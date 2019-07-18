8d5972b1c010290f1c76640df9601f8d2d7d69b2
Anon <arebarozz@gmail.com>
2019-07-18 15:35:04 +0700

    done and crossdoxk

--- ProjectTAP/Engine/Business/Issued/ProcessQueueCrossDock.cs
+++ ProjectTAP/Engine/Business/Issued/ProcessQueueCrossDock.cs
@@ -20,79 +20,82 @@ namespace ProjectTAP.Engine.Business.Issued
             {
                 var crossdockItem = reqDoc.DocumentItems.Where(docitem => docitem.DocItemStos.Any(disto => disto.BaseQuantity < docitem.BaseQuantity)).ToList();
 
-                amt_Document doc = new amt_Document()
+                if(crossdockItem.Count > 0)
                 {
-                    ID = null,
-                    Code = null,
-                    ParentDocument_ID = reqDoc.ID.Value,
-                    Lot = reqDoc.Lot,
-                    Batch = null,
-                    For_Customer_ID = null,
-                    Sou_Customer_ID = null,
-                    Sou_Supplier_ID = null,
-                    Sou_Branch_ID = reqDoc.Sou_Warehouse_ID,
-                    Sou_Warehouse_ID = reqDoc.Sou_Warehouse_ID,
-                    Sou_AreaMaster_ID = null,
-
-                    Des_Customer_ID = null,
-                    Des_Supplier_ID = null,
-                    Des_Branch_ID = reqDoc.Des_Warehouse_ID,
-                    Des_Warehouse_ID = reqDoc.Des_Warehouse_ID,
-                    Des_AreaMaster_ID = null,
-                    DocumentDate = DateTime.Now,
-                    ActionTime = null,
-                    MovementType_ID = MovementType.FG_CROSSDOCK_CUS,
-                    RefID = null,
-                    Ref1 = null,
-                    Ref2 = null,
-
-                    DocumentType_ID = DocumentTypeID.GOODS_RECEIVED,
-                    EventStatus = DocumentEventStatus.NEW,
-
-                    Remark = null,
-                    Transport_ID = null,
-
-                    DocumentItems = new List<amt_DocumentItem>(),
-
-                };
-                crossdockItem.ForEach(item =>
-                {
-                    var pack = AWMSEngine.ADO.DataADO.GetInstant().SelectByCodeActive<ams_PackMaster>(item.Code, buVO);
-                    var sumQtyDisto = item.DocItemStos.Sum(disto => disto.BaseQuantity);
-                    if (sumQtyDisto < item.BaseQuantity)
+                    amt_Document doc = new amt_Document()
                     {
-                        doc.DocumentItems.Add(new amt_DocumentItem()
+                        ID = null,
+                        Code = null,
+                        ParentDocument_ID = reqDoc.ID.Value,
+                        Lot = reqDoc.Lot,
+                        Batch = null,
+                        For_Customer_ID = null,
+                        Sou_Customer_ID = null,
+                        Sou_Supplier_ID = null,
+                        Sou_Branch_ID = reqDoc.Sou_Warehouse_ID,
+                        Sou_Warehouse_ID = reqDoc.Sou_Warehouse_ID,
+                        Sou_AreaMaster_ID = null,
+
+                        Des_Customer_ID = null,
+                        Des_Supplier_ID = null,
+                        Des_Branch_ID = reqDoc.Des_Warehouse_ID,
+                        Des_Warehouse_ID = reqDoc.Des_Warehouse_ID,
+                        Des_AreaMaster_ID = null,
+                        DocumentDate = DateTime.Now,
+                        ActionTime = null,
+                        MovementType_ID = MovementType.FG_CROSSDOCK_CUS,
+                        RefID = null,
+                        Ref1 = null,
+                        Ref2 = null,
+
+                        DocumentType_ID = DocumentTypeID.GOODS_RECEIVED,
+                        EventStatus = DocumentEventStatus.NEW,
+
+                        Remark = null,
+                        Transport_ID = null,
+
+                        DocumentItems = new List<amt_DocumentItem>(),
+
+                    };
+                    crossdockItem.ForEach(item =>
+                    {
+                        var pack = AWMSEngine.ADO.DataADO.GetInstant().SelectByCodeActive<ams_PackMaster>(item.Code, buVO);
+                        var sumQtyDisto = item.DocItemStos.Sum(disto => disto.BaseQuantity);
+                        if (sumQtyDisto < item.BaseQuantity)
                         {
-                            ID = null,
-                            Code = item.Code,
-                            SKUMaster_ID = pack.SKUMaster_ID,
-                            PackMaster_ID = pack.ID,
+                            doc.DocumentItems.Add(new amt_DocumentItem()
+                            {
+                                ID = null,
+                                Code = item.Code,
+                                SKUMaster_ID = pack.SKUMaster_ID,
+                                PackMaster_ID = pack.ID,
 
-                            Quantity = item.BaseQuantity - sumQtyDisto,
-                            UnitType_ID = item.UnitType_ID,
-                            BaseQuantity = item.BaseQuantity - sumQtyDisto,
-                            BaseUnitType_ID = item.BaseUnitType_ID,
+                                Quantity = item.BaseQuantity - sumQtyDisto,
+                                UnitType_ID = item.UnitType_ID,
+                                BaseQuantity = item.BaseQuantity - sumQtyDisto,
+                                BaseUnitType_ID = item.BaseUnitType_ID,
 
-                            OrderNo = item.OrderNo,
-                            Batch = item.Batch,
-                            Lot = item.Lot,
+                                OrderNo = item.OrderNo,
+                                Batch = item.Batch,
+                                Lot = item.Lot,
 
-                            Options = item.Options,
-                            ExpireDate = item.ExpireDate,
-                            ProductionDate = item.ProductionDate,
-                            Ref1 = item.Ref1,
-                            Ref2 = item.Ref2,
-                            RefID = item.RefID,
+                                Options = item.Options,
+                                ExpireDate = item.ExpireDate,
+                                ProductionDate = item.ProductionDate,
+                                Ref1 = item.Ref1,
+                                Ref2 = item.Ref2,
+                                RefID = item.RefID,
 
-                            EventStatus = DocumentEventStatus.NEW,
+                                EventStatus = DocumentEventStatus.NEW,
 
-                        });
-                    }
-                });
+                            });
+                        }
+                    });
 
-                var docID = AWMSEngine.ADO.DocumentADO.GetInstant().Create(doc, buVO).ID;
+                    var docID = AWMSEngine.ADO.DocumentADO.GetInstant().Create(doc, buVO).ID;
 
-                resDoc.Add(AWMSEngine.ADO.DocumentADO.GetInstant().Get(docID.Value, buVO));
+                    resDoc.Add(AWMSEngine.ADO.DocumentADO.GetInstant().Get(docID.Value, buVO));
+                }
             });
 
             return resDoc;
