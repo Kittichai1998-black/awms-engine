afdfe6a3e758ff24c52195e8e9e855ebb966f1da
Nukaiang <nitchakan_2530@hotmail.com>
2019-07-19 15:42:43 +0700

    Fix Rename Upper

--- AWMSEngine/Engine/V2/Business/WorkQueue/ASRSProcessQueue.cs
+++ AWMSEngine/Engine/V2/Business/WorkQueue/ASRSProcessQueue.cs
@@ -75,8 +75,8 @@ namespace AWMSEngine.Engine.V2.Business.WorkQueue
             TRes res = new TRes()
             {
                 processResults = new List<TRes.ProcessQueueResult>(),
-                DesASRSWarehouseCode = reqVO.desASRSWarehouseCode,
-                DesASRSAreaCode = reqVO.desASRSAreaCode,
+                desASRSWarehouseCode = reqVO.desASRSWarehouseCode,
+                desASRSAreaCode = reqVO.desASRSAreaCode,
                 desASRSLocationCode = reqVO.desASRSLocationCode
             };
             var desASRSWm = this.StaticValue.Warehouses.First(x => x.Code == reqVO.desASRSWarehouseCode);
