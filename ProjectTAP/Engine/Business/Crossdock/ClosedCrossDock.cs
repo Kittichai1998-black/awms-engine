37208fe8127122c5a06c3075866634dba9f1221c
Anon <arebarozz@gmail.com>
2019-07-22 15:50:15 +0700

    fix crossdock

---
+++ ProjectTAP/Engine/Business/Crossdock/ClosedCrossDock.cs
@@ -0,0 +1,24 @@
+﻿using AWMSEngine.Engine;
+using AWMSModel.Constant.EnumConst;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectTAP.Engine.Business.Crossdock
+{
+    public class ClosedCrossDock : BaseEngine<long, string>
+    {
+        protected override string ExecuteEngine(long reqVO)
+        {
+            var DocumentADO = AWMSEngine.ADO.DocumentADO.GetInstant();
+            var getGRDoc = DocumentADO.Get(reqVO, this.BuVO);
+            var getGIDoc = DocumentADO.Get(getGRDoc.ParentDocument_ID.Value, this.BuVO);
+
+            DocumentADO.UpdateStatusToChild(getGRDoc.ID.Value, null, null, DocumentEventStatus.CLOSED, this.BuVO);
+            DocumentADO.UpdateStatusToChild(getGIDoc.ID.Value, null, null, DocumentEventStatus.CLOSED, this.BuVO);
+
+            return null;
+        }
+    }
+}
