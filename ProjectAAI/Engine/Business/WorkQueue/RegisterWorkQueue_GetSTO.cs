0046bfe038d049aca91f12fba49cab66b3ecb991
Fhay_949 <6100949@DBF02-AMW618364>
2019-08-01 10:00:25 +0700

    regis workq AAI

--- ProjectAAI/Engine/Business/WorkQueue/RegisterWorkQueue_GetSTO.cs
+++ ProjectAAI/Engine/Business/WorkQueue/RegisterWorkQueue_GetSTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using AMWUtil.Common;
@@ -14,62 +15,13 @@ using AWMSModel.Criteria;
 using AWMSModel.Entity;
 using ProjectAAI.ADO;
 using ProjectAAI.ADO.SAPApi;
+using static ProjectAAI.ADO.SAPApi.SAPCriteria;
+using static ProjectAAI.ADO.SAPApi.SAPInterfaceADO;
 
 namespace ProjectAAI.Engine.Business.WorkQueue
 {
     public class RegisterWorkQueue_GetSTO : IProjectEngine<RegisterWorkQueue.TReq, StorageObjectCriteria>
     {
-        public class SAPResponse
-        {
-            public List<ZSWMRF001_OUT_SU> datas;
-            public int status;
-            public string message;
-            public string stacktrace;
-            public class ZSWMRF001_OUT_SU
-            {
-                /// <summary>Warehouse Code</summary>
-                public string LGNUM;
-                /// <summary>Storage Type</summary>
-                public string LGTYP;
-                /// <summary>Storage Unit Number</summary>
-                public string LENUM;
-                /// <summary>Material Number</summary>
-                public string MATNR;
-                /// <summary>Batch Number</summary>
-                public string CHARG;
-                /// <summary>Stock Category</summary>
-                public string BESTQ;
-                /// <summary>Available Stock</summary>
-                public decimal VERME;
-                /// <summary>Base Unit of Measure</summary>
-                public string MEINS;
-                /// <summary>Date of Manufacture</summary>
-                public string HSDAT;
-                /// <summary>Incubated Time</summary>
-                public double WEBAZ;
-                /// <summary>Gross Weight</summary>
-                public decimal BRGEW;
-                /// <summary>Weight Unit</summary>
-                public string GEWEI;
-                /// <summary>approved batch number </summary>
-                public string FVDT1;
-                /// <summary>Error message</summary>
-                ///public string VBELN;
-                /// <summary>Error message</summary>
-                public string ERR_MSG;
-            }
-        }
-        
-        private class SAPReq
-        {
-            public string environmentName;
-            public string functionName;
-            public string inStructureName;
-            public string inTableName;
-            public string outTableName;
-            public IDictionary<string, object> datas;
-        }
-
         public StorageObjectCriteria ExecuteEngine(AMWLogger logger, VOCriteria buVO, RegisterWorkQueue.TReq reqVO)
         {
             var chkStos = AWMSEngine.ADO.StorageObjectADO.GetInstant().Get(reqVO.baseCode, null, null, false, true, buVO);
@@ -83,8 +35,6 @@ namespace ProjectAAI.Engine.Business.WorkQueue
 
                 if (_base == null)
                 {
-
-                   
                     var BaseMasterType = StaticValueManager.GetInstant().BaseMasterTypes.FirstOrDefault();
                     ams_BaseMaster newBase = new ams_BaseMaster()
                     {
@@ -98,8 +48,12 @@ namespace ProjectAAI.Engine.Business.WorkQueue
                     };
                     var idbase = AWMSEngine.ADO.DataADO.GetInstant().Insert<ams_BaseMaster>(buVO, newBase);
                     _base = AWMSEngine.ADO.DataADO.GetInstant().SelectByID<ams_BaseMaster>(idbase, buVO);
+                    if (_base == null)
+                    {
+                        throw new AMWException(logger, AMWExceptionCode.V1001, "Not Found Pallet : " + reqVO.baseCode);
+                    }
+
                 }
-                //throw new AMWException(logger, AMWExceptionCode.V1001, "Not Found Pallet : " + reqVO.baseCode);
                 var _unitType = StaticValueManager.GetInstant().UnitTypes.FirstOrDefault(x => x.ID == _base.UnitType_ID);
                 var _objSize = StaticValueManager.GetInstant().ObjectSizes.FirstOrDefault(x => x.ObjectType == StorageObjectType.BASE);
 
@@ -129,8 +83,7 @@ namespace ProjectAAI.Engine.Business.WorkQueue
                 };
 
                 var baseStoID = AWMSEngine.ADO.StorageObjectADO.GetInstant().PutV2(baseSto, buVO);
-               // packCode.ForEach(packcode =>
-               // {
+                
                     var packList = GetObjectFromSAP(reqVO.baseCode, reqVO.warehouseCode, buVO);
                     packList.datas.ForEach(pack =>
                     {
@@ -148,17 +101,19 @@ namespace ProjectAAI.Engine.Business.WorkQueue
                             throw new AMWException(logger, AMWExceptionCode.V2001, "Unit Type " + pack.MEINS + " Not Found"); // MEINS : Base Unit of Measure
                         if(_sku.UnitType_ID != unit.ID)
                             throw new AMWException(logger, AMWExceptionCode.V2001, "Unit Type " + pack.MEINS + " Not Match"); // MEINS : Base Unit of Measure
+                        var _objSizePack = StaticValueManager.GetInstant().ObjectSizes.Find(x => x.ID == _pack.ObjectSize_ID);
+                        //var _objSizePack = StaticValueManager.GetInstant().ObjectSizes.FirstOrDefault(x => x.ObjectType == StorageObjectType.PACK);
 
-                        var _objSizePack = StaticValueManager.GetInstant().ObjectSizes.FirstOrDefault(x => x.ObjectType == StorageObjectType.PACK);
-                        DateTime productDate = DateTime.Parse(pack.HSDAT); 
-                        DateTime incubatedate = productDate.AddDays(pack.WEBAZ);
-                        //DateTime? productDate = (DateTime?)pack.HSDAT.Get<DateTime>();
-                        //var incubatedate = (DateTime?)productDate.AddDays(36)<DateTime>();
+                        DateTime productDate = DateTime.ParseExact(pack.HSDAT,"yyyyMMdd", CultureInfo.InvariantCulture); //"20190527"
+                        DateTime incubatedate = productDate.AddDays(pack.WEBAZ - 1); 
+                        var incb = DateTimeUtil.ToISOUTCString(incubatedate);
+                        DateTime fvdt1 = DateTime.ParseExact(pack.FVDT1, "yyyyMMdd", CultureInfo.InvariantCulture);
+                        var approveddate = DateTimeUtil.ToISOUTCString(fvdt1);
                         var options = ObjectUtil.QryStrSetValue(null, 
                             new KeyValuePair<string, object>("bestq", pack.BESTQ), //Stock Category 
                             new KeyValuePair<string, object>("webaz", pack.WEBAZ), //Incubated Time
-                            new KeyValuePair<string, object>("incubatedate", incubatedate),
-                            new KeyValuePair<string, object>("fvdt1", pack.FVDT1) //approved date 
+                            new KeyValuePair<string, object>("incubatedate", incb),
+                            new KeyValuePair<string, object>("fvdt1", approveddate) //approved date 
                             );
 
                         StorageObjectCriteria packSto = new StorageObjectCriteria()
@@ -185,34 +140,20 @@ namespace ProjectAAI.Engine.Business.WorkQueue
                             weiKG = WeightUtil.ConvertToKG(pack.BRGEW, pack.GEWEI), //BRGEW : Gross Weight, GEWEI : Weight Unit
                             areaID = StaticValueManager.GetInstant().AreaMasters.FirstOrDefault(x => x.Code == reqVO.areaCode).ID.Value,
                             refID = pack.LENUM, //SU : Storage Unit Number
-                            ref1 = pack.FVDT1 != null ? "y" : null //ถ้ามีค่า approved date ให้ใส่เป็น y ถ้าไม่มีใส่ null
+                            ref1 = pack.FVDT1 != null || pack.FVDT1.Length > 0 ? "y" : null //ถ้ามีค่า approved date ให้ใส่เป็น y ถ้าไม่มีใส่ null
                         };
 
                         AWMSEngine.ADO.StorageObjectADO.GetInstant().PutV2(packSto, buVO);
                     });
-               // });
-
+                
                 var mapsto = AWMSEngine.ADO.StorageObjectADO.GetInstant().Get(baseStoID, StorageObjectType.BASE, false, true, buVO);
                 return mapsto;
             }
         }
 
-        private SAPResponse GetObjectFromSAP(string barcode, string warehouse, VOCriteria buVO)
+        private SapResponse<ZSWMRF001_OUT_SU> GetObjectFromSAP(string barcode, string warehouse, VOCriteria buVO)
         {
-
-            var list = new Dictionary<string, object>();
-            list.Add("LGNUM", "W01");
-            list.Add("LENUM", barcode);
-            var itemList = new SAPReq()
-            {
-                environmentName = "DEV",
-                functionName = "ZWMRF001",
-                inStructureName = "ZSWMRF001_IN_SU",
-                inTableName = "IN_SU",
-                outTableName = "OUT_SU",
-                datas = list
-            };
-            var res = SAPInterfaceADO.GetInstant().postSAP<SAPResponse>(itemList, buVO, "http://localhost:51306/api/SAPConnect");
+            var res = SAPInterfaceADO.GetInstant().ZWMRF001(barcode, buVO);
             return res;
         }
     }
