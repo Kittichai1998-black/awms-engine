2a3149aa8d40152ae968dcd2d31d259f2c8cc970
Ple <6100871@BDF01-AMW618311>
2019-09-12 10:24:34 +0700

    commit

--- WMS_Frontend/src/views/page/Pankan/Loading/Loading.js
+++ WMS_Frontend/src/views/page/Pankan/Loading/Loading.js
@@ -72,7 +72,7 @@ const FormInline = styled.div`
 
 const LabelH = styled.label`
   font-weight: bold;
-  width: 100px;
+  width: 150px;
   paddingleft: 20px;
 `;
 
@@ -91,29 +91,95 @@ const Loading = props => {
     l: 100,
     all: ""
   };
+  const onHandleDDLChange = (value, dataObject, fieldDataKey) => {
+    // setValueText1({
+    //   ...valueText1,
+    //   [inputID]: {
+    //     value: value,
+    //     dataObject: dataObject,
+    //     fieldDataKey: fieldDataKey
+    //   }
+    // });
+    console.log(value);
+    onGetData(value);
+  };
+
+  function onGetData(value) {
+    console.log(value);
+    const data = {
+      docID: value
+    };
+
+    Axios.post(window.apipath + "/v2/ListLoadingConsoAPI", data).then(res => {
+      console.log(res);
+
+      //console.log(groupdata);
+      // let groupdisplay = [];
+      // let packname = [];
+      // for (let row in groupdata) {
+      //   groupdata[row].forEach(grow => {
+      //     packname.forEach((prow, index) => {
+      //       if (prow === grow.packName) packname.splice(index, 1);
+      //     });
+      //     packname.push(grow.packName);
+      //   });
+      //   let result = groupdata[row][0];
+      //   result.item = packname.join(",");
+      //   groupdisplay.push(groupdata[row][0]);
+      //   packname = [];
+      // }
+      //this.setState({ data: groupdisplay });
+    });
+  }
+
   return (
     <div className={classes.root}>
       <Paper className={classes.paperContainer}>
-        <FormInline>
-          <LabelH>Document : </LabelH>
-          <InputDiv>
-            <AmDropdown
-              id={"issuedDocID"}
-              placeholder={"Select Document"}
-              fieldDataKey="ID"
-              fieldLabel={["Code"]}
-              labelPattern=" : "
-              width={300}
-              ddlMinWidth={300}
-              zIndex={1000}
-              queryApi={DocumentQuery}
-              //   onChange={(value, dataObject, field) =>
-              //     onHandleDDLChangeDoc(value, dataObject, field)
-              //   }
-              ddlType={"search"}
-            />
-          </InputDiv>
+        <FormInline
+          style={{
+            display: "-webkit-inline-box"
+          }}
+        >
+          <LabelH>Loading Document : </LabelH>
+
+          <AmDropdown
+            id={"issuedDocID"}
+            placeholder={"Select Document"}
+            fieldDataKey="ID"
+            fieldLabel={["Code"]}
+            labelPattern=" : "
+            width={200}
+            ddlMinWidth={200}
+            zIndex={1000}
+            queryApi={DocumentQuery}
+            onChange={(value, dataObject, field) =>
+              onHandleDDLChange(value, dataObject, field)
+            }
+            ddlType={"search"}
+          />
         </FormInline>
+
+        <LabelH>Transport : </LabelH>
+
+        <br />
+        <FormInline
+          style={{
+            display: "-webkit-inline-box"
+          }}
+        >
+          <LabelH>Barcode : </LabelH>
+
+          <AmInput
+            id={"cols.field"}
+            style={{ width: "120px" }}
+            type="input"
+
+            //onChange={(val)=>{onChangeEditor(cols.field, data, val,"Pack Name")}}
+          />
+        </FormInline>
+        <AmButton styleType="default" style={{ marginLeft: "20px" }}>
+          {"Back"}
+        </AmButton>
       </Paper>
     </div>
   );
