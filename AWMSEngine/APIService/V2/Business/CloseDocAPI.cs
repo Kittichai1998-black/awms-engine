34bcf04bde8fd9f61485446e8b3b6c0341d31f5e
Ple <6100871@BDF01-AMW618311>
2019-09-17 14:06:39 +0700

    commit

--- AWMSEngine/APIService/V2/Business/CloseDocAPI.cs
+++ AWMSEngine/APIService/V2/Business/CloseDocAPI.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AWMSEngine.Engine.V2.Business.WorkQueue;
+using AWMSModel.Constant.EnumConst;
 
 namespace AWMSEngine.APIService.V2.ASRS
 {
@@ -21,12 +22,41 @@ namespace AWMSEngine.APIService.V2.ASRS
 
         protected override dynamic ExecuteEngineManual()
         {
-            this.BeginTransaction();
-            TReq req = AMWUtil.Common.ObjectUtil.DynamicToModel<TReq>(this.RequestVO);
-            var resClosed = new ClosedDocument().Execute(this.Logger, this.BuVO, req.docIDs);
-            this.CommitTransaction();
+            List<long> res = new List<long>();
 
-            return resClosed;
+            TReq reqDoc = AMWUtil.Common.ObjectUtil.DynamicToModel<TReq>(this.RequestVO);
+            reqDoc.docIDs.ForEach(doc =>
+            {
+            var docs = ADO.DocumentADO.GetInstant().Get(doc, this.BuVO);
+            if (docs != null)
+            {
+                if (docs.EventStatus == DocumentEventStatus.NEW)
+                {
+                    //เอา error ไปเก็บไว้ใน options
+                    BuVO.FinalLogDocMessage.Add(new AWMSModel.Criteria.FinalDatabaseLogCriteria.DocumentOptionMessage(
+                        docs.ID.Value, null, null, "Document Eventstatus is NEW"
+                    ));
+                }else{
+                    this.BeginTransaction();
+                    TReq req = AMWUtil.Common.ObjectUtil.DynamicToModel<TReq>(this.RequestVO);
+                    var resWorked = new WorkedDocument().Execute(this.Logger, this.BuVO, req.docIDs);
+                    this.CommitTransaction();
+
+                    this.BeginTransaction();
+                    TReq reqClosing = AMWUtil.Common.ObjectUtil.DynamicToModel<TReq>(this.RequestVO);
+                    var resClosing = new ClosingDocument().Execute(this.Logger, this.BuVO, reqClosing.docIDs);
+                    this.CommitTransaction();
+
+                    this.BeginTransaction();
+                    TReq reqClosed = AMWUtil.Common.ObjectUtil.DynamicToModel<TReq>(this.RequestVO);
+                    var resClosed = new ClosedDocument().Execute(this.Logger, this.BuVO, reqClosed.docIDs);
+
+                    res.AddRange(resClosed);
+                    }
+                }
+            });
+
+            return res;
         }
 
     }
