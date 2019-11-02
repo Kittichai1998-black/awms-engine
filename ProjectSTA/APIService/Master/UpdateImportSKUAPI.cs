5b25b97e7664e5670b3dd2151953acacee4633c6
Ple <6100871@BDF01-AMW618311>
2019-11-02 16:10:37 +0700

    commit

---
+++ ProjectSTA/APIService/Master/UpdateImportSKUAPI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AMWUtil.Common;
+using Microsoft.AspNetCore.Mvc;
+using ProjectSTA.Engine.Business;
+
+namespace ProjectSTA.APIService.Master
+{
+    public class UpdateImportSKUAPI : AWMSEngine.APIService.BaseAPIService
+    {
+        public UpdateImportSKUAPI(ControllerBase controllerAPI,int apiServiceID = 0, bool isAuthenAuthorize = true) : base(controllerAPI, apiServiceID, isAuthenAuthorize)
+        {
+        }
+
+        protected override dynamic ExecuteEngineManual()
+        {
+            this.BeginTransaction();
+            var req = ObjectUtil.DynamicToModel<UpdateImportSKU.TDocReq >(this.RequestVO);
+            var res = new UpdateImportSKU().Execute(this.Logger, this.BuVO, req);
+
+            return res;
+        }
+    }
+}
