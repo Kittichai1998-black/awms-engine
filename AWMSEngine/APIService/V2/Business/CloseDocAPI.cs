a19191a26a60aea1887b3cd25f206d24a1b99332
Ple <6100871@BDF01-AMW618311>
2019-11-18 17:39:36 +0700

    commit

--- AWMSEngine/APIService/V2/Business/CloseDocAPI.cs
+++ AWMSEngine/APIService/V2/Business/CloseDocAPI.cs
@@ -40,7 +40,7 @@ namespace AWMSEngine.APIService.Business
                 }else{
                         //this.BeginTransaction();                       
                         TReq req = AMWUtil.Common.ObjectUtil.DynamicToModel<TReq>(this.RequestVO);
-                        if (docs.EventStatus == DocumentEventStatus.CLOSING)
+                        if (docs.EventStatus == DocumentEventStatus.CLOSING || docs.EventStatus == DocumentEventStatus.WORKED)
                         {
                             var resClosed = new ClosedDocument().Execute(this.Logger, this.BuVO, req.docIDs);
            
