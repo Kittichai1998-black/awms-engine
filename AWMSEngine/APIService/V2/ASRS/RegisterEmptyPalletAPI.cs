47c1053d9c6ec72d4c56ea5b1035777519799dc6
Anon Phupui <anon_katoon@hotmail.com>
2019-12-04 14:40:42 +0700

    add reg empty pallet

---
+++ AWMSEngine/APIService/V2/ASRS/RegisterEmptyPalletAPI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AWMSEngine.Engine.V2.Business.WorkQueue;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWMSEngine.APIService.V2.ASRS
+{
+    public class RegisterEmptyPalletAPI : BaseAPIService
+    {
+        public RegisterEmptyPalletAPI(ControllerBase controllerAPI, int apiServiceID = 0, bool isAuthenAuthorize = true) : base(controllerAPI, apiServiceID, isAuthenAuthorize)
+        {
+        }
+
+        protected override dynamic ExecuteEngineManual()
+        {
+            this.BeginTransaction();
+
+            RegisterWorkQueue.TReq req = AMWUtil.Common.ObjectUtil.DynamicToModel<RegisterWorkQueue.TReq>(this.RequestVO);
+            var engine = new Engine.V2.Business.WorkQueue.RegisterEmptyPallet();
+            var res = engine.Execute(this.Logger, this.BuVO, req);
+            return res;
+        }
+    }
+}
---
+++ AWMSEngine/Engine/V2/Business/WorkQueue/RegisterEmptyPallet.cs
@@ -0,0 +1,43 @@
+﻿using AWMSModel.Criteria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWMSEngine.Engine.V2.Business.WorkQueue
+{
+    public class RegisterEmptyPallet : BaseEngine<RegisterWorkQueue.TReq, WorkQueueCriteria>
+    {
+        protected override WorkQueueCriteria ExecuteEngine(RegisterWorkQueue.TReq reqVO)
+        {
+            var regQueue = new RegisterWorkQueue();
+            var resRegQueue = regQueue.Execute(this.Logger, this.BuVO, reqVO);
+
+            var workingData = new WorkingStageQueue.TReq()
+            {
+                warehouseCode= reqVO.desAreaCode,
+                actualTime= DateTime.Now,
+                areaCode= reqVO.desAreaCode,
+                baseCode= reqVO.baseCode,
+                locationCode= reqVO.desLocationCode,
+                queueID= resRegQueue.queueID
+            };
+            var workingQueue = new WorkingStageQueue();
+            var resWorkingQueue = workingQueue.Execute(this.Logger, this.BuVO, workingData);
+
+            var doneData = new DoneQ.TReq()
+            {
+                warehouseCode = resWorkingQueue.desWarehouseCode,
+                actualTime = DateTime.Now,
+                areaCode = resWorkingQueue.desAreaCode,
+                baseCode = resWorkingQueue.baseInfo.baseCode,
+                locationCode = resWorkingQueue.desLocationCode,
+                queueID = resWorkingQueue.queueID
+            };
+            var doneQueue = new DoneQ();
+            doneQueue.Execute(this.Logger, this.BuVO, doneData);
+
+            return resRegQueue;
+        }
+    }
+}
