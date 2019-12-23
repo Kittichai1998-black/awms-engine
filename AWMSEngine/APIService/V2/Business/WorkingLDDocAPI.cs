848dd2ca363bafba00bd681599a9efd6e1a40e9b
Ple <6100871@BDF01-AMW618311>
2019-12-23 14:39:51 +0700

    commit

---
+++ AWMSEngine/APIService/V2/Business/WorkingLDDocAPI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AMWUtil.Common;
+using AWMSEngine.Engine.Business;
+using AWMSEngine.Engine.Business.Loading;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWMSEngine.APIService.V2.Business
+{
+    public class WorkingLDDocAPI : BaseAPIService
+    { 
+        public WorkingLDDocAPI(ControllerBase controllerAPI,int apiServiceID = 0, bool isAuthenAuthorize = true) : base(controllerAPI, apiServiceID, isAuthenAuthorize)
+        {
+        }
+
+        protected override dynamic ExecuteEngineManual()
+        {
+            this.BeginTransaction();
+            var req = ObjectUtil.DynamicToModel<WorkingLDDocument.TDocReq>(this.RequestVO);
+            var res = new WorkingLDDocument().Execute(this.Logger, this.BuVO, req);
+
+            return res;
+        }
+    }
+}
