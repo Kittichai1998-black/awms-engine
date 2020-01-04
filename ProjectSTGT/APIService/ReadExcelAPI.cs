40bd5295adefc11c05b4b7293f9fd41b11da65d4
Anon Phupui <anon_katoon@hotmail.com>
2020-01-04 12:51:54 +0700

    fix excel reader

---
+++ ProjectSTGT/APIService/ReadExcelAPI.cs
@@ -0,0 +1,19 @@
+﻿using AWMSEngine.APIService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSTGT.APIService
+{
+    public class ReadExcelAPI : BaseAPIService
+    {
+        protected override dynamic ExecuteEngineManual()
+        {
+            this.BeginTransaction();
+            var engine = new ProjectSTGT.Engine.Worker.ReadExcelFile();
+            engine.Execute(this.Logger, this.BuVO, null);
+            return null;
+        }
+    }
+}
--- ProjectSTGT/Engine/WM/ReadExcelFile.cs
+++ ProjectSTGT/Engine/WM/ReadExcelFile.cs
@@ -11,6 +11,7 @@ using AWMSModel.Entity;
 using AWMSEngine.Engine.V2.Business.Received;
 using AWMSModel.Constant.EnumConst;
 using AWMSModel.Criteria;
+using System.Dynamic;
 
 namespace ProjectSTGT.Engine.WM
 {
@@ -34,83 +35,21 @@ namespace ProjectSTGT.Engine.WM
             {
                 while (true)
                 {
-                    var dbTrx = AWMSEngine.ADO.DataADO.GetInstant().CreateTransaction();
-                    VOCriteria buVO = new VOCriteria(dbTrx);
                     try
                     {
-                        ReadFileFromDirectory();
+                        var createGRDoc = new ProjectSTGT.APIService.ReadExcelAPI();
+                        var res = createGRDoc.Execute(new ExpandoObject());
                     }
-                    catch {
+                    catch (Exception ex)
+                    {
                     }
                     finally
                     {
-                        Thread.Sleep(3000);
+                        Thread.Sleep(10000);
                     }
                 }
             });
             return tsk;
-        }
-
-        private void ReadFileFromDirectory()
-        {
-            var directoryPath = AWMSEngine.ADO.StaticValue.StaticValueManager.GetInstant().Configs.FirstOrDefault(x => x.Code == "DIRECTORY_PATH").DataValue;
-            var resList = new List<TRes>();
-            var getFile = new DirectoryInfo(directoryPath).GetFiles();
-            foreach (var file in getFile)
-            {
-                try
-                {
-                    var res = FilesTypeAccess.ExcelAccess(file);
-                    CreateDocument(res);
-                    file.MoveTo($"{directoryPath}\\Archive\\{file.Name}");
-                }
-                catch
-                {
-                }
-            }
-        }
-
-        private void CreateDocument(AMWUtil.DataAccess.FilesTypeAccess.ExcelDataResponse excelData)
-        {
-            excelData.worksheet.ForEach(data =>
-            {
-                var docItems = new List<CreateGRDocument.TReq.ReceiveItem>();
-                data.rows.Skip(1).ToList().ForEach(row =>
-                {
-                    var status = AMWUtil.Common.EnumUtil.GetValueEnum<StorageObjectEventStatus>(row.cells[6]);
-                    docItems.Add(new CreateGRDocument.TReq.ReceiveItem()
-                    {
-                        skuCode = row.cells[0],
-                        orderNo = row.cells[1],
-                        options = $"carton_no={row.cells[5]}&saleorder={row.cells[2]}&status={status}&remark={row.cells[7]}",
-                        quantity = Convert.ToDecimal(row.cells[3]),
-                        unitType = row.cells[4]
-                    });
-                });
-
-                var gDocItems = docItems.GroupBy(docItem => new { docItem.skuCode }).Select(key => new { sku = key.Key, docItems = key.ToList() }).ToList();
-
-                gDocItems.ForEach(x =>
-                {
-                    try
-                    {
-                        var doc = new CreateGRDocument.TReq();
-
-                        doc.documentDate = DateTime.Now;
-                        doc.eventStatus = DocumentEventStatus.NEW;
-                        doc.movementTypeID = MovementType.FG_TRANSFER_WM;
-                        doc.receiveItems = new List<CreateGRDocument.TReq.ReceiveItem>();
-                        doc.receiveItems = x.docItems;
-
-                        var createGRDoc = new AWMSEngine.APIService.Doc.CreateGRDocAPI(null, 0, false);
-                        var res = createGRDoc.Execute(doc);
-                    }
-                    catch
-                    {
-                        return;
-                    }
-                });
-            });
         }
     }
 }
