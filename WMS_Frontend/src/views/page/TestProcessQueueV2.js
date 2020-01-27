9aa62c5e3539c15260ee2a886cfbadb43626490a
Anon Phupui <anon_katoon@hotmail.com>
2020-01-27 10:03:34 +0700

    add pcqV2

---
+++ WMS_Frontend/src/views/page/TestProcessQueueV2.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import AmProcessQueue from "../pageComponent/AmProcessQueue/AmProcessQueue";
+
+const ProcessQueue = () => {
+  return <AmProcessQueue/>
+}
+
+export default ProcessQueue;
