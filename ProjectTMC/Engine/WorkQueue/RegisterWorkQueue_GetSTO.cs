d6f9389c316e2d28b8b557435116a698b04c6ca5
Ple <6100871@BDF01-AMW618311>
2020-02-24 09:39:33 +0700

    Edit reg TMC

--- ProjectTMC/Engine/WorkQueue/RegisterWorkQueue_GetSTO.cs
+++ ProjectTMC/Engine/WorkQueue/RegisterWorkQueue_GetSTO.cs
@@ -55,9 +55,26 @@ namespace ProjectTMC.Engine.WorkQueue
                 });
             }
             //==========================================================
+            amt_Document docGR = new amt_Document();
+            List<amt_DocumentItem> docGRItems = new List<amt_DocumentItem>();
+
+            //Inbound Zone
+            docGR = AWMSEngine.ADO.DataADO.GetInstant().SelectBy<amt_Document>(
+                new SQLConditionCriteria[] {
+                        new SQLConditionCriteria("DocumentProcessType_ID",4010, SQLOperatorType.EQUALS),
+                        new SQLConditionCriteria("EventStatus","10,11",SQLOperatorType.IN)
+            }, buVO).FirstOrDefault();
+
+            docGRItems = AWMSEngine.ADO.DataADO.GetInstant().SelectBy<amt_DocumentItem>(
+                new SQLConditionCriteria[] {
+                        new SQLConditionCriteria("Document_ID",docGR.ID, SQLOperatorType.EQUALS),
+                        new SQLConditionCriteria("EventStatus","10,11",SQLOperatorType.IN),
+            }, buVO);
 
             if (reqVO.mappingPallets != null && reqVO.mappingPallets.Count > 0)
             {
+               
+
                 foreach (var mappingPallet in reqVO.mappingPallets)
                 {//Check Qty
                     if (reqVO.areaCode == "R")
@@ -66,7 +83,7 @@ namespace ProjectTMC.Engine.WorkQueue
                         if (mappingPallet.qty <= 3)
                         {
                             stos = this.mapPallet(logger, reqVO, null, buVO);
-                            this.mapDisto(logger, stos, buVO);
+                            this.mapDisto(logger, stos, docGRItems, buVO);
                         }
                         else
                         {
@@ -81,7 +98,7 @@ namespace ProjectTMC.Engine.WorkQueue
                         if (mappingPallet.qty <= 16)
                         {
                             stos = this.mapPallet(logger, reqVO, null, buVO);
-                            this.mapDisto(logger, stos, buVO);
+                            this.mapDisto(logger, stos, docGRItems, buVO);
                         }
                         else
                         {
@@ -95,24 +112,9 @@ namespace ProjectTMC.Engine.WorkQueue
             else
             {
                 //No mappingPallets
-                amt_Document docGR = new amt_Document();
-                List<amt_DocumentItem> docGRItems = new List<amt_DocumentItem>();
-
-                    //Inbound Zone
-                    docGR = AWMSEngine.ADO.DataADO.GetInstant().SelectBy<amt_Document>(
-                        new SQLConditionCriteria[] {
-                        new SQLConditionCriteria("DocumentProcessType_ID",4010, SQLOperatorType.EQUALS),
-                        new SQLConditionCriteria("EventStatus","10,11",SQLOperatorType.IN)
-                    }, buVO).FirstOrDefault();
-
-                    docGRItems = AWMSEngine.ADO.DataADO.GetInstant().SelectBy<amt_DocumentItem>(
-                        new SQLConditionCriteria[] {
-                        new SQLConditionCriteria("Document_ID",docGR.ID, SQLOperatorType.EQUALS),
-                        new SQLConditionCriteria("EventStatus","10,11",SQLOperatorType.IN),
-                    }, buVO);
 
                     stos = this.mapPallet(logger, reqVO, docGRItems, buVO);
-                    this.mapDisto(logger, stos, buVO);
+                    this.mapDisto(logger, stos, docGRItems, buVO);
 
             }
 
@@ -218,23 +220,28 @@ namespace ProjectTMC.Engine.WorkQueue
 
             return mapsto;
         }
-        private List<amt_DocumentItemStorageObject> mapDisto(AMWLogger logger, StorageObjectCriteria data, VOCriteria buVO)
+        private List<amt_DocumentItemStorageObject> mapDisto(AMWLogger logger, StorageObjectCriteria data, List<amt_DocumentItem> dataDoc, VOCriteria buVO)
         {
             // Map data to Table DocumentStorageObject
             var stoMap = data.ToTreeList().Find(x => x.type == StorageObjectType.PACK);
 
-            var disto = new amt_DocumentItemStorageObject
+            var disto = new List <amt_DocumentItemStorageObject>();
+            foreach (var docItem in dataDoc)
             {
-                ID = null,
-                DocumentItem_ID = null,
-                Sou_StorageObject_ID = stoMap.id.Value,
-                Des_StorageObject_ID = stoMap.id.Value,
-                Quantity = stoMap.qty,
-                BaseQuantity = stoMap.baseQty,
-                UnitType_ID = stoMap.unitID,
-                BaseUnitType_ID = stoMap.baseUnitID,
-                Status = EntityStatus.ACTIVE
+                disto.Add(new amt_DocumentItemStorageObject()
+                {
+                    ID = null,
+                    DocumentItem_ID = docItem.ID,
+                    Sou_StorageObject_ID = stoMap.id.Value,
+                    Des_StorageObject_ID = stoMap.id.Value,
+                    Quantity = stoMap.qty,
+                    BaseQuantity = stoMap.baseQty,
+                    UnitType_ID = stoMap.unitID,
+                    BaseUnitType_ID = stoMap.baseUnitID,
+                    Status = EntityStatus.ACTIVE
+                });
             };
+
 
             AWMSEngine.ADO.DocumentADO.GetInstant().InsertMappingSTO(disto, buVO);
 
