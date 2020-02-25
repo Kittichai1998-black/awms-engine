bc2cc78fe2d344706ddbdc181f175501c5ddacf5
Woramet <dexter_top@hotmail.com>
2020-02-25 14:56:15 +0700

    page download log

---
+++ AWMSEngine/APIService/V2/Data/GetRootLogFileAPI.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AWMSEngine.Engine.V2.General;
+
+namespace AWMSEngine.APIService.V2.Data
+{
+    public class GetRootLogFileAPI : BaseAPIService
+    {
+        public GetRootLogFileAPI(ControllerBase controllerAPI, int apiServiceID = 0, bool isAuthenAuthorize = true) : base(controllerAPI, apiServiceID, isAuthenAuthorize)
+        {
+        }
+
+        protected override dynamic ExecuteEngineManual()
+        {
+            this.BeginTransaction();
+            var resLogDirectory = new GetRootLogFile().Execute(this.Logger, this.BuVO, null);
+            //this.CommitTransaction();
+            return resLogDirectory;
+        }
+    }
+}
