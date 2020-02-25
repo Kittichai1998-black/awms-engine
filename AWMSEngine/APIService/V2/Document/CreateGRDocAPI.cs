823995eaec5af1ef363be4102fa2464770c43116
Ple <6100871@BDF01-AMW618311>
2020-02-25 15:18:27 +0700

    commit download LogFile

---
+++ AWMSEngine/APIService/V2/Document/CreateGRDocAPI.cs
@@ -0,0 +1,28 @@
+﻿using AMWUtil.Common;
+using AWMSEngine.Engine.V2.Business.Received;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWMSEngine.APIService.V2.Document
+{
+    public class CreateGRDocAPI : BaseAPIService
+    {
+        public CreateGRDocAPI(ControllerBase controllerAPI,int apiServiceID = 0, bool isAuthenAuthorize = true) : base(controllerAPI, apiServiceID, isAuthenAuthorize)
+        {
+        }
+
+        protected override dynamic ExecuteEngineManual()
+        {
+            this.BeginTransaction();
+            var reqDoc = ObjectUtil.DynamicToModel<CreateGRDocument.TReq>(this.RequestVO);
+            var res = new CreateGRDocument().Execute(
+                this.Logger,
+                this.BuVO,
+                reqDoc);
+            return res;
+        }
+    }
+}
