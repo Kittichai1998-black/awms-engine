c4d7ca4cca26f4503f1e2d3e7f9428c70ab92f57
Fhay_949 <6100949@DBF02-AMW618364>
2020-08-10 15:11:40 +0700

    update sto

--- AWMSModel/Criteria/StorageObjectCriteria_Method.cs
+++ AWMSModel/Criteria/StorageObjectCriteria_Method.cs
@@ -313,9 +313,9 @@ namespace AWMSModel.Criteria
         {
             var s = this;
             return EncryptUtil.GenerateMD5(
-                        string.Format("{0}|{1}|{2}|{3}|{4}|{5}|{6}|{7}|{8}|{9}|{10}|{11:dd-mm-yy}|{12:dd-mm-yy}",
-                            s.mstID, s.type, s.forCustomerID, s.orderNo, s.batch, s.lot,
-                            s.refID, s.ref1, s.ref2, s.ref3, s.ref4, 
+                        string.Format("{0}|{1}|{2}|{3}|{4}|{5}|{6}|{7}|{8}|{9}|{10:dd-mm-yy}|{11:dd-mm-yy}",
+                            s.mstID, s.type, s.forCustomerID, s.orderNo, s.batch,
+                            s.lot, s.ref1, s.ref2, s.ref3, s.ref4, 
                             s.productDate, s.expiryDate));
         }
 
