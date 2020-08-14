5f129c4f2e4b95c28465e02d1800c2bc50482bfb
Ple <6100871@BDF01-AMW618311>
2020-08-14 08:58:27 +0700

    commit auditStatus

---
+++ WMS_Frontend/src/components/Models/AuditStatus.js
@@ -0,0 +1,9 @@
+const AuditStatus = [
+  { label: "QUARANTINE", value: 0 },
+  { label: "PASS", value: 1 },
+  { label: "NOTPASS", value: 2 },
+  { label: "HOLD", value: 9 }
+
+];
+
+export { AuditStatus }
