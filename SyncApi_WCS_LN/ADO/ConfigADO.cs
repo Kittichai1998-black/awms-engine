7ec2a3469c12c9bc374be481caffa04e2622d36b
Rattana Oudtagosum <memo_love02@hotmail.com>
2020-08-18 08:48:15 +0700

    StoEventStatus & WCS PTTOR api center

---
+++ SyncApi_WCS_LN/ADO/ConfigADO.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SyncApi_WCS_LN.ADO
+{
+    public static class ConfigADO
+    {
+        public static Dictionary<string,string> DatatbaseConfigs { get; set; }
+
+        public static Dictionary<string, string> Post2wmsConfigs { get; set; }
+        public static Dictionary<string, string> ApiControllerConfigs { get; set; }
+        /*
+        public static string Post_ConsoloadUpdate_sp_post { get; set; }
+        public static string Post_ConsoloadUpdate_sp_result { get; set; }
+        public static string Post_ConsoloadUpdate_api_url { get; set; }
+
+        public static string Post_SelectReturnUpdate_sp_post { get; set; }
+        public static string Post_SelectReturnUpdate_sp_result { get; set; }
+        public static string Post_SelectReturnUpdate_api_url { get; set; }
+
+        public static string Post_StorageInUpdate_sp_post { get; set; }
+        public static string Post_StorageInUpdate_sp_result { get; set; }
+        public static string Post_StorageInUpdate_api_url { get; set; }
+
+        public static string Post_StorageMovement_sp_post { get; set; }
+        public static string Post_StorageMovement_sp_result { get; set; }
+        public static string Post_StorageMovement_api_url { get; set; }
+
+        public static string Post_StorageOutUpdate_sp_post { get; set; }
+        public static string Post_StorageOutUpdate_sp_result { get; set; }
+        public static string Post_StorageOutUpdate_api_url { get; set; }
+
+        public static string Post_StoragePickUpdate_sp_post { get; set; }
+        public static string Post_StoragePickUpdate_sp_result { get; set; }
+        public static string Post_StoragePickUpdate_api_url { get; set; }
+
+        public static string Post_StoreAuditUpdate_sp_post { get; set; }
+        public static string Post_StoreAuditUpdate_sp_result { get; set; }
+        public static string Post_StoreAuditUpdate_api_url { get; set; }
+        */
+    }
+}
