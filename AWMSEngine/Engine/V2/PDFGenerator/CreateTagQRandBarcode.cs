bed369b4a06f752b0a090b93021680514032b77b
Fhay_949 <6100949@DBF02-AMW618364>
2020-08-22 10:57:11 +0700

    update code pdf

--- AWMSEngine/Engine/V2/PDFGenerator/CreateTagQRandBarcode.cs
+++ AWMSEngine/Engine/V2/PDFGenerator/CreateTagQRandBarcode.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using AMWUtil.Common;
+using AWMSModel.Constant.EnumConst;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
 
@@ -22,28 +23,9 @@ namespace AWMSEngine.Engine.V2.PDFGenerator
             public string code;
             public string title;
             public string options;
-            //public CodeType codeType;
-
+            public SKUGroupType skuType;
         }
-        //public class PageSize
-        //{
-        //    public float width;
-        //    public float height;
-        //    public float marginLeft;
-        //    public float marginRight;
-        //    public float marginTop;
-        //    public float marginBottom;
-        //}
-        //public class TagSize
-        //{
-        //    public double width;
-        //    public double height; 
-        //}
-        //public enum CodeType
-        //{
-        //    BARCODE = 0,
-        //    QRCODE = 1
-        //}
+      
         public enum LayoutType
         {
             LANDSCAPE_BARCODE = 0,
@@ -52,7 +34,7 @@ namespace AWMSEngine.Engine.V2.PDFGenerator
             PORTRAIT_QR = 3,
             CUSTOM1 = 91,
         }
-
+        
         public class TRes
         {
             public byte[] stream;
@@ -67,9 +49,12 @@ namespace AWMSEngine.Engine.V2.PDFGenerator
             public const string OPT_CONTROL_NO = "controlNo";
             public const string OPT_SUPPLIER = "supplier";
             public const string OPT_CODENO = "codeNo";
-            public const string OPT_RECEIVED_DATE = "receivedDate";
-            public const string OPT_QTY_RECEIVED = "qtyReceived";
+            public const string OPT_MFG_DATE = "mfgdate";
+            public const string OPT_EXP_DATE = "expdate";
+            public const string OPT_QTY = "qty";
+            public const string OPT_UNIT = "unit"; 
             public const string OPT_PALLET_NO = "palletNo";
+            public const string OPT_REMARK = "remark";
         }
         private Font h1;
         private Font h2;
@@ -87,7 +72,7 @@ namespace AWMSEngine.Engine.V2.PDFGenerator
         private Font p7;
         public BaseFont CreateFont(string fontName)
         {
-            string fg = Path.Combine(Environment.CurrentDirectory + "\\assets\\", fontName);
+            string fg = Path.Combine(Environment.CurrentDirectory + "\\assets\\fonts\\", fontName);
             return BaseFont.CreateFont(fg, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
         }
         public void GenerateFont()
@@ -179,7 +164,7 @@ namespace AWMSEngine.Engine.V2.PDFGenerator
         {
             listsCode.ForEach(info => {
                 document.Add(GetHeaderQRCustom1(info.title, info.code));
-                document.Add(GetDetailCustom1(info.options));
+                document.Add(GetDetailCustom1(info.skuType, info.options));
                 document.NewPage();
             });
 
@@ -236,7 +221,17 @@ namespace AWMSEngine.Engine.V2.PDFGenerator
             headerTable.SetWidths(headerTableColWidth);
             headerTable.LockedWidth = true;
 
-            PdfPCell headerTableCell_0 = new PdfPCell(new Phrase(title, h1));
+            iTextSharp.text.Image pnglogo = iTextSharp.text.Image.GetInstance(Environment.CurrentDirectory + "\\assets\\images\\logo_boss.png");
+            pnglogo.ScaleAbsolute(78f, 37f);
+            PdfPCell headerTableCell = new PdfPCell(pnglogo);
+            headerTableCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            headerTableCell.Border = Rectangle.NO_BORDER;
+            headerTableCell.Colspan = 2;
+            headerTableCell.PaddingBottom = 5f;
+            headerTable.AddCell(headerTableCell);
+
+            Font _font = FontFactory.GetFont(BaseFont.HELVETICA_BOLD, 24, Font.NORMAL);
+            PdfPCell headerTableCell_0 = new PdfPCell(new Phrase(title, _font));
             headerTableCell_0.HorizontalAlignment = Element.ALIGN_CENTER;
             headerTableCell_0.VerticalAlignment = Element.ALIGN_MIDDLE;
             headerTable.AddCell(headerTableCell_0);
@@ -253,83 +248,130 @@ namespace AWMSEngine.Engine.V2.PDFGenerator
             
             return headerTable;
         }
-        private PdfPTable GetDetailCustom1(string details)
+        private PdfPTable GetDetailCustom1(SKUGroupType skuType, string details)
         {
             var itemName = ObjectUtil.QryStrGetValue(details, OptionConst.OPT_ITEMNAME);
             var lotNo = ObjectUtil.QryStrGetValue(details, OptionConst.OPT_LOT_NO);
             var controlNo = ObjectUtil.QryStrGetValue(details, OptionConst.OPT_CONTROL_NO);
             var supplier = ObjectUtil.QryStrGetValue(details, OptionConst.OPT_SUPPLIER);
             var codeNo = ObjectUtil.QryStrGetValue(details, OptionConst.OPT_CODENO);
-            var receivedDate = ObjectUtil.QryStrGetValue(details, OptionConst.OPT_RECEIVED_DATE);
-            var qtyReceived = ObjectUtil.QryStrGetValue(details, OptionConst.OPT_QTY_RECEIVED);
+            var mfgDate = ObjectUtil.QryStrGetValue(details, OptionConst.OPT_MFG_DATE);
+            var expDate = ObjectUtil.QryStrGetValue(details, OptionConst.OPT_EXP_DATE);
+            var qtyReceived = ObjectUtil.QryStrGetValue(details, OptionConst.OPT_QTY);
             var palletNo = ObjectUtil.QryStrGetValue(details, OptionConst.OPT_PALLET_NO);
-            
-            PdfPTable table = new PdfPTable(4);
+            var unit = ObjectUtil.QryStrGetValue(details, OptionConst.OPT_UNIT);
+            var remark = ObjectUtil.QryStrGetValue(details, OptionConst.OPT_REMARK);
+            PdfPTable table = new PdfPTable(6);
             table.TotalWidth = 406f;
             table.HorizontalAlignment = 0;
             table.DefaultCell.Border = Rectangle.BOX;
-            //table.SpacingBefore = 5;
-            //table.SpacingAfter = 5;
 
-            float[] tableColWidth = new float[4];
-            tableColWidth[0] = 80f;
-            tableColWidth[1] = 173f;
-            tableColWidth[2] = 63f;
-            tableColWidth[3] = 90f;
+            float[] tableColWidth = new float[6];
+            tableColWidth[0] = 60f;
+            tableColWidth[1] = 76f;
+            tableColWidth[2] = 70f;
+            tableColWidth[3] = 60f;
+            tableColWidth[4] = 40f;
+            tableColWidth[5] = 90f;
             table.SetWidths(tableColWidth);
             table.LockedWidth = true;
             //row1
-            PdfPCell tableCell_0 = new PdfPCell(new Phrase("Item Name ", h7));
+            PdfPCell tableCell_0 = new PdfPCell(new Phrase("Code no.", h7));
             table.AddCell(SetHeaderCol(tableCell_0));
-
-            PdfPCell tableCell_1 = new PdfPCell(new Phrase(itemName, p7));
-            tableCell_1.Colspan = 3;
+            PdfPCell tableCell_1 = new PdfPCell(new Phrase(codeNo, p7));
             table.AddCell(SetInfoCol(tableCell_1));
 
-            //row 2
-            tableCell_0 = new PdfPCell(new Phrase("Lot no. ", h7));
-            table.AddCell(SetHeaderCol(tableCell_0));
+            PdfPCell tableCell_2 = new PdfPCell(new Phrase("Item Name ", h7));
+            tableCell_2.HorizontalAlignment = Element.ALIGN_RIGHT;
+            tableCell_2 = SetHeaderCol(tableCell_2);
+            table.AddCell(tableCell_2);
 
-            tableCell_1 = new PdfPCell(new Phrase(lotNo, p7));
-            tableCell_1.Colspan = 3;
-            table.AddCell(SetInfoCol(tableCell_1));
+            PdfPCell tableCell_3 = new PdfPCell(new Phrase(itemName, p7));
+            tableCell_3.Colspan = 3;
+            table.AddCell(SetInfoCol(tableCell_3));
 
-            //row3
-            tableCell_0 = new PdfPCell(new Phrase("Control no. ", h7));
-            table.AddCell(SetHeaderCol(tableCell_0));
+            //PdfPCell tableCell_4 = new PdfPCell(new Phrase(itemName, p7));
+            //table.AddCell(SetInfoCol(tableCell_4));
 
-            tableCell_1 = new PdfPCell(new Phrase(controlNo, p7));
-            tableCell_1.Colspan = 3;
-            table.AddCell(SetInfoCol(tableCell_1));
+            if (skuType == SKUGroupType.FG)
+            {
+                //row 2
+                tableCell_0 = new PdfPCell(new Phrase("Lot no. ", h7));
+                table.AddCell(SetHeaderCol(tableCell_0));
+
+                tableCell_1 = new PdfPCell(new Phrase(lotNo, p7));
+                tableCell_1.Colspan = 5;
+                table.AddCell(SetInfoCol(tableCell_1));
+ 
+                //row3
+                tableCell_0 = new PdfPCell(new Phrase("Mfg. Date ", h7));
+                table.AddCell(SetHeaderCol(tableCell_0));
+
+                tableCell_1 = new PdfPCell(new Phrase(mfgDate, p7));
+                tableCell_1.Colspan = 2;
+                table.AddCell(SetInfoCol(tableCell_1));
+
+                tableCell_2 = new PdfPCell(new Phrase("Exp. Date ", h7));
+                table.AddCell(SetHeaderCol(tableCell_2));
+
+                tableCell_3 = new PdfPCell(new Phrase(expDate, p7));
+                tableCell_3.Colspan = 2;
+                table.AddCell(SetInfoCol(tableCell_3));
+
+            }
+            else {
+                //row 2
+                tableCell_0 = new PdfPCell(new Phrase("Lot Vendor ", h7));
+                table.AddCell(SetHeaderCol(tableCell_0));
+
+                tableCell_1 = new PdfPCell(new Phrase(lotNo, p7));
+                tableCell_1.Colspan = 2;
+                table.AddCell(SetInfoCol(tableCell_1));
+
+                tableCell_2 = new PdfPCell(new Phrase("Control no. ", h7));
+                table.AddCell(SetHeaderCol(tableCell_2));
+
+                tableCell_3 = new PdfPCell(new Phrase(controlNo, p7));
+                tableCell_3.Colspan = 2;
+                table.AddCell(SetInfoCol(tableCell_3));
+
+                //row3
+                tableCell_0 = new PdfPCell(new Phrase("Supplier ", h7));
+                table.AddCell(SetHeaderCol(tableCell_0));
+
+                tableCell_1 = new PdfPCell(new Phrase(supplier, p7));
+                tableCell_1.Colspan = 5;
+                table.AddCell(SetInfoCol(tableCell_1));
+
+            }
+
             //row4
-            tableCell_0 = new PdfPCell(new Phrase("Supplier ", h7));
+            tableCell_0 = new PdfPCell(new Phrase("Quantity ", h7));
             table.AddCell(SetHeaderCol(tableCell_0));
 
-            tableCell_1 = new PdfPCell(new Phrase(supplier, p7));
+            tableCell_1 = new PdfPCell(new Phrase(qtyReceived, p7)); 
+            tableCell_1.Colspan = 2;
             table.AddCell(SetInfoCol(tableCell_1));
 
-            PdfPCell tableCell_2 = new PdfPCell(new Phrase("Code no. ", h7));
+            tableCell_2 = new PdfPCell(new Phrase("Unit ", h7));
             table.AddCell(SetHeaderCol(tableCell_2));
 
-            PdfPCell tableCell_3 = new PdfPCell(new Phrase(codeNo, p7));
+            tableCell_3 = new PdfPCell(new Phrase(unit, p7));
+            tableCell_3.Colspan = 2;
             table.AddCell(SetInfoCol(tableCell_3));
 
             //row5
-            tableCell_0 = new PdfPCell(new Phrase("Quantity Received ", h7));
+            tableCell_0 = new PdfPCell(new Phrase("Remark ", h7));
             table.AddCell(SetHeaderCol(tableCell_0));
 
-            tableCell_1 = new PdfPCell(new Phrase(qtyReceived, p7));
+            tableCell_1 = new PdfPCell(new Phrase(remark, p7));
             tableCell_1.Colspan = 3;
             table.AddCell(SetInfoCol(tableCell_1));
-            //row6
-            tableCell_0 = new PdfPCell(new Phrase("Received Date ", h7));
-            table.AddCell(SetHeaderCol(tableCell_0));
 
-            tableCell_1 = new PdfPCell(new Phrase(receivedDate, p7));
-            table.AddCell(SetInfoCol(tableCell_1));
-
-            tableCell_2 = new PdfPCell(new Phrase("Pallet No. ", h7));
-            table.AddCell(SetHeaderCol(tableCell_2));
+            tableCell_2 = new PdfPCell(new Phrase("No. ", h7));
+            tableCell_2 = SetHeaderCol(tableCell_2);
+            tableCell_2.HorizontalAlignment = Element.ALIGN_RIGHT;
+            table.AddCell(tableCell_2);
 
             tableCell_3 = new PdfPCell(new Phrase(palletNo, p7));
             table.AddCell(SetInfoCol(tableCell_3));
