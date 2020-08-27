89314f5eae74dec2486d4cd7419025cac974dcc3
Fhay_949 <6100949@DBF02-AMW618364>
2020-08-27 13:30:13 +0700

    update processtype

---
+++ AWMSModel/Entity/amv_DocumentProcessMap.cs
@@ -0,0 +1,15 @@
+﻿
+using AWMSModel.Constant.EnumConst;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AWMSModel.Entity
+{
+    public class amv_DocumentProcessMap : ams_DocumentProcessType
+    {
+        public DocumentProcessTypeID DocumentProcessType_ID;
+        public DocumentTypeID DocumentType_ID;
+        public string ReProcessType_Name;
+    }
+}
