a90b545887f89bb42d2af3bc6cce1747c31eb4fb
Anon Phupui <anon_katoon@hotmail.com>
2020-08-27 08:59:56 +0700

    add action in table and custom pagesize for page

---
+++ WMS_Frontend/src/components/AmDropDownMenu.js
@@ -0,0 +1,18 @@
+import React, {useRef} from "react";
+import {Dropdown} from "react-bootstrap";
+
+const AmDropdownMenu = (props) => {
+    const ref = useRef();
+    return <Dropdown style={props.style}>
+        <Dropdown.Toggle as={props.customToggle ? props.customToggle : undefined} id={props.id ? 'dropdownMenu' : props.id}>
+            {props.title}
+        </Dropdown.Toggle>
+        <Dropdown.Menu ref={ref} as={props.customItems ? props.customItems : undefined}>
+            {props.items.map(item => {
+                return <Dropdown.Item as="button" onClick={() => item.action(props.datas)}>{item.label}</Dropdown.Item>
+            })}
+        </Dropdown.Menu>
+  </Dropdown>
+}
+
+export default AmDropdownMenu;
--- WMS_Frontend/src/views/page/test2.js
+++ WMS_Frontend/src/views/page/test2.js
@@ -6,76 +6,17 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import AmDropdownNMenu from '../../components/AmDropDownMenu';
 
-const styles = theme => ({
-    root: {
-      width: '100%',
-      marginTop: theme.spacing(3),
-      overflowX: 'auto',
-    },
-    table: {
-      minWidth: 700,
-    },
-  });
-  
-  let id = 0;
+export default (props) => {
+    const items = [
+        {label:"Test1", action:(data) => console.log(data)},
+        {label:"Test2", action:(data) => alert("Test2")},
+        {label:"Test3", action:(data) => alert("Test3")},
+        {label:"Test4", action:(data) => alert("Test4")},
+        {label:"Test5", action:(data) => alert("Test5")}
+    ];
+    const datas = [{id:1, value:'1'},{id:2, value:'2'},{id:3, value:'3'},{id:4, value:'4'},{id:5, value:'5'},{id:6, value:'6'}]
 
-  function createData(name, calories, fat, lname, protein) {
-    id += 1;
-    return { id, name, calories, fat, lname, protein };
-  }
-  const rows = [
-    createData('Frozen yoghurtFrozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-  ];
-
-  const columns = [{"field":"name", "Header":"Name", "fieldType":"Text", "Cell":null},
-  {"field":"lname", "Header":"Last Name", "fieldType":"Text", "Cell":null},
-  {"field":"age", "Header":"Age", "fieldType":"Text", "Cell":null},
-  {"field":"height", "Header":"Height", "fieldType":"Text", "Cell":null},
-  {"field":"weight", "Header":"Weight", "fieldType":"Text", "Cell":null}]
-
-class Test2 extends Component{
-    render(){
-        const {classes} = this.props;
-        return (
-            <div>
-
-        <Table className={classes.table}>
-            <TableHead>
-
-            <TableRow>
-                {columns.map((x,idx) => {
-                    return <TableCell key={idx}>{x.Header}</TableCell>
-                })}
-            </TableRow>
-            </TableHead>
-            <TableBody>
-            {rows.map((x,idx) => {
-                return <TableRow key={x.id}>
-                {columns.map((y,idx2) => {
-                    let element = []
-                    Object.keys(x).forEach((z,idx3) => {
-                        if(y.field === z){
-                            if(y.Cell === null){
-                                element.push(<TableCell key={idx3} component="th" scope="row">{x[z]}</TableCell>)
-                            }else{
-                                element.push(<TableCell key={idx3} component="th" scope="row">{x[z]}</TableCell>)
-                            }
-                        }
-                    })
-                    return element
-                })}
-                </TableRow>
-            })}
-            </TableBody>
-                </Table>
-                </div>
-        )
-    }
+    return <AmDropdownNMenu items={items} datas={datas} title="Action"/>
 }
-
-export default withStyles(styles)(Test2);
