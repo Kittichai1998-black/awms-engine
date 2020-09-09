52df72b0623fdb7be1e2002fe512b03c2f7a97bc
Ple <6100871@BDF01-AMW618311>
2020-09-09 14:50:43 +0700

    rj doc

---
+++ AWMSEngine/Engine/V2/Business/Document/RejectDocument.cs
@@ -0,0 +1,39 @@
+﻿using AMWUtil.Common;
+using AMWUtil.Exception;
+using AWMSEngine.ADO;
+using AWMSEngine.ADO.StaticValue;
+using AWMSEngine.Common;
+using AWMSModel.Constant.EnumConst;
+using AWMSModel.Constant.StringConst;
+using AWMSModel.Criteria;
+using AWMSModel.Entity;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace AWMSEngine.Engine.V2.Business.Document
+{
+    public class RejectDocument : BaseEngine<RejectDocument.TReq, RejectDocument.TRes>
+    {
+        public class TReq
+        {
+            public List<long> docIDs;
+            public string remark;
+            public string docType;
+        }
+
+        public class TRes
+        {
+            public List<amt_Document> documents;
+        }
+
+        protected override TRes ExecuteEngine(TReq reqVO)
+        {
+
+            return null;
+        }
+    }
+}
