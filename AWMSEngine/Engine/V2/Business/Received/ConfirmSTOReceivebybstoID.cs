3ff4e3b14179bf39bc837de41b7334125f29af91
Ple <6100871@BDF01-AMW618311>
2020-10-02 14:01:14 +0700

    fix receive on floor

--- AWMSEngine/Engine/V2/Business/Received/ConfirmSTOReceivebybstoID.cs
+++ AWMSEngine/Engine/V2/Business/Received/ConfirmSTOReceivebybstoID.cs
@@ -29,11 +29,15 @@ namespace AWMSEngine.Engine.V2.Business.Received
 
             var disto = AWMSEngine.ADO.DataADO.GetInstant().SelectBy<amt_DocumentItemStorageObject>(
                 new KeyValuePair<string, object>[] {
-                        new KeyValuePair<string,object>("Sou_StorageObject_ID",pack),                       
+                        new KeyValuePair<string,object>("Sou_StorageObject_ID",pack),
                 }, this.BuVO).FirstOrDefault();
-            
+
 
             var docID = ADO.DataADO.GetInstant().SelectByID<amt_DocumentItem>(disto.DocumentItem_ID, this.BuVO).Document_ID;
+            var docItemID = AWMSEngine.ADO.DataADO.GetInstant().SelectBy<amt_DocumentItem>(
+                      new KeyValuePair<string, object>[] {
+                        new KeyValuePair<string,object>("Document_ID",docID),
+                      }, this.BuVO);
             var statusDocID = ADO.DataADO.GetInstant().SelectByID<amt_Document>(docID, this.BuVO).Status;
 
             if (statusDocID != EntityStatus.ACTIVE)
@@ -43,67 +47,52 @@ namespace AWMSEngine.Engine.V2.Business.Received
             if (docs == null)
                 throw new AMWException(this.Logger, AMWExceptionCode.V1001, "ไม่พบข้อมูล Document");
 
-            if (docs.DocumentType_ID == DocumentTypeID.PUTAWAY)
+            var docItem = AWMSEngine.ADO.DataADO.GetInstant().SelectBy<amt_DocumentItem>(
+                     new KeyValuePair<string, object>[] {
+                        new KeyValuePair<string,object>("Document_ID",docs.ID.Value),
+                     }, this.BuVO);
+
+
+            ADO.StorageObjectADO.GetInstant().UpdateStatus(disto.Sou_StorageObject_ID, null, null, StorageObjectEventStatus.RECEIVED, BuVO);
+            //update Audit status, Hold status
+
+            //set_status_base(stosPack.parentID.Value, stosPack.parentType.Value);
+            disto.Status = EntityStatus.ACTIVE;
+            ADO.DistoADO.GetInstant().Update(disto.ID.Value, EntityStatus.ACTIVE, BuVO);
+            //ถ้าไม่มี des_waveseq สุดท้ายเเล้ว ให้อัพเดท disto เป็น Done
+            if (disto.Des_WaveSeq_ID == null)
             {
-                if (docs.EventStatus == DocumentEventStatus.NEW || docs.EventStatus == DocumentEventStatus.WORKING)
+                disto.Status = EntityStatus.DONE;
+                ADO.DistoADO.GetInstant().Update(disto.ID.Value, EntityStatus.DONE, this.BuVO);
+            }
+
+
+            //if (item.DocItemStos.Any(x => x.Status == EntityStatus.ACTIVE || x.Status == EntityStatus.DONE))
+            //{
+            //    item.EventStatus = DocumentEventStatus.WORKING;
+            //    ADO.DocumentADO.GetInstant().UpdateItemEventStatus(item.ID.Value, DocumentEventStatus.WORKING, this.BuVO);
+            //}
+
+
+
+
+            ADO.DocumentADO.GetInstant().UpdateEventStatus(docID, DocumentEventStatus.WORKING, this.BuVO);
+
+            var getGR = ADO.DocumentADO.GetInstant().GetDocumentAndDocItems(docs.ParentDocument_ID.Value, this.BuVO);
+
+            docItem.ForEach(item =>
+            {
+                var grItem = getGR.DocumentItems.Find(y => y.ID == item.ParentDocumentItem_ID);
+                if (item.EventStatus == DocumentEventStatus.WORKING)
                 {
-                    var docitemsto = ADO.DocumentADO.GetInstant().ListItemAndDisto(docID, BuVO);
-                    docitemsto.ForEach(item =>
-                    {
-                        //decimal sumQtyDisto = item.DocItemStos.Sum(z => z.BaseQuantity ?? 0);
-                        //decimal totalQty = item.BaseQuantity ?? 0;
-                        //if (sumQtyDisto != totalQty)
-                        //    throw new AMWException(this.Logger, AMWExceptionCode.V1001, "จำนวนสินค้าของรายการ SKU: " + item.Code + " ที่ต้องการรับเข้าไม่ตรงกับจำนวนที่ระบุในเอกสาร");
-                        if(item.EventStatus == DocumentEventStatus.NEW)
-                        {
-                            item.DocItemStos.ForEach(disto =>
-                            {
-                                ADO.StorageObjectADO.GetInstant().UpdateStatus(disto.Sou_StorageObject_ID, null, null, StorageObjectEventStatus.RECEIVED, BuVO);
-                                //update Audit status, Hold status
-
-                                //set_status_base(stosPack.parentID.Value, stosPack.parentType.Value);
-                                disto.Status = EntityStatus.ACTIVE;
-                                ADO.DistoADO.GetInstant().Update(disto.ID.Value, EntityStatus.ACTIVE, BuVO);
-                                //ถ้าไม่มี des_waveseq สุดท้ายเเล้ว ให้อัพเดท disto เป็น Done
-                                if (disto.Des_WaveSeq_ID == null)
-                                {
-                                    disto.Status = EntityStatus.DONE;
-                                    ADO.DistoADO.GetInstant().Update(disto.ID.Value, EntityStatus.DONE, this.BuVO);
-                                }
-                            });
-
-                            if (item.DocItemStos.Any(x => x.Status == EntityStatus.ACTIVE || x.Status == EntityStatus.DONE))
-                            {
-                                item.EventStatus = DocumentEventStatus.WORKING;
-                                ADO.DocumentADO.GetInstant().UpdateItemEventStatus(item.ID.Value, DocumentEventStatus.WORKING, this.BuVO);
-                            }
-                        }
-                    });
-
-
-                    ADO.DocumentADO.GetInstant().UpdateEventStatus(docID, DocumentEventStatus.WORKING, this.BuVO);
-
-                    var getGR = ADO.DocumentADO.GetInstant().GetDocumentAndDocItems(docs.ParentDocument_ID.Value, this.BuVO);
-
-                    docitemsto.ForEach(item =>
-                    {
-                        var grItem = getGR.DocumentItems.Find(y => y.ID == item.ParentDocumentItem_ID);
-                        if(item.EventStatus == DocumentEventStatus.WORKING)
-                        {
-                            grItem.EventStatus = DocumentEventStatus.WORKING;
-                            ADO.DocumentADO.GetInstant().UpdateItemEventStatus(grItem.ID.Value, DocumentEventStatus.WORKING, this.BuVO);
-                        }
-                    });
-
-                    ADO.DocumentADO.GetInstant().UpdateEventStatus(docs.ParentDocument_ID.Value, DocumentEventStatus.WORKING, this.BuVO);
-
+                    grItem.EventStatus = DocumentEventStatus.WORKING;
+                    ADO.DocumentADO.GetInstant().UpdateItemEventStatus(grItem.ID.Value, DocumentEventStatus.WORKING, this.BuVO);
                 }
-            }
-            else
-            {
-                throw new AMWException(this.Logger, AMWExceptionCode.V1001, "ไม่ใช่เอกสารรับเข้าไม่สามารถทำการปิดเอกสารได้");
+            });
 
-            }
+            ADO.DocumentADO.GetInstant().UpdateEventStatus(docs.ParentDocument_ID.Value, DocumentEventStatus.WORKING, this.BuVO);
+
+
             res.docID = docID;
             return res;
         }
