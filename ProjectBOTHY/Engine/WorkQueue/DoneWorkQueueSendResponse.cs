0870f19fe3543c8797657a1a97b7ee15f0102be7
Anon <arebarozz@gmail.com>
2020-11-02 16:17:17 +0700

    add file

---
+++ ProjectBOTHY/Engine/WorkQueue/DoneWorkQueueSendResponse.cs
@@ -0,0 +1,19 @@
+﻿using AMWUtil.Logger;
+using AWMSEngine.Engine;
+using AWMSEngine.Engine.V2.Business.WorkQueue;
+using AWMSModel.Criteria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectBOTHY.Engine.WorkQueue
+{
+    public class DoneWorkQueueSendResponse : IProjectEngine<DoneWorkQueue.TReq, WorkQueueCriteria>
+    {
+        public WorkQueueCriteria ExecuteEngine(AMWLogger logger, VOCriteria buVO, DoneWorkQueue.TReq reqVO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
