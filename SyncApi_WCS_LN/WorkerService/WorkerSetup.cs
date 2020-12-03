cd74ed50155cb1c0a0d19a42775ea3cffff4833c
Woramet <dexter_top@hotmail.com>
2020-12-03 19:11:28 +0700

    api wcs

--- SyncApi_WCS_LN/WorkerService/WorkerSetup.cs
+++ SyncApi_WCS_LN/WorkerService/WorkerSetup.cs
@@ -16,9 +16,9 @@ namespace SyncApi_WCS_LN.WorkerService
             string[] api_names = ConfigADO.Post2wmsConfigs[ConfigString.KEY_POST2WMS_APINAMES].Split(",");
             foreach(var api_name in api_names)
             {
-                string api_url = ConfigADO.Post2wmsConfigs[string.Format(ConfigString.KEY_POST2WMS_APIURL, api_name)];
-                string sp_request = ConfigADO.Post2wmsConfigs[string.Format(ConfigString.KEY_POST2WMS_SP_REQUEST, api_name)];
-                string sp_response = ConfigADO.Post2wmsConfigs[string.Format(ConfigString.KEY_POST2WMS_SP_RESPONSE, api_name)];
+                string api_url = ConfigADO.Post2wmsConfigs[string.Format(ConfigString.KEY_POST2WMS_APIURL, api_name.ToLower())];
+                string sp_request = ConfigADO.Post2wmsConfigs[string.Format(ConfigString.KEY_POST2WMS_SP_REQUEST, api_name.ToLower())];
+                string sp_response = ConfigADO.Post2wmsConfigs[string.Format(ConfigString.KEY_POST2WMS_SP_RESPONSE, api_name.ToLower())];
                 var exec = new WorkerWCS2WMS(sp_request, sp_response, api_url);
                 Task t = Task.Run(() =>
                 {
