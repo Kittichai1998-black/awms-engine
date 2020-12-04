66e60ff60f16bd1e4261744945da62d554ff3ad7
Woramet <dexter_top@hotmail.com>
2020-12-04 11:45:08 +0700

    ptt wcs - fix initInstant log

--- SyncApi_WCS_LN/Startup.cs
+++ SyncApi_WCS_LN/Startup.cs
@@ -19,6 +19,8 @@ namespace SyncApi_WCS_LN
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            AMWUtil.Logger.AMWLoggerManager.InitInstant("D:/logs/wcs_ptt/{Date}/", "{LogName}.{Date}.log");
+
             using (StreamReader sr = new StreamReader("app.property"))
             {
                 while (sr.EndOfStream == false)
