6f54443aa57d2f0faa857124d5627472e07295ee
sumonrut <sumonrut@kkumail.com>
2021-03-01 20:54:46 +0700

    commit Mccommand

--- AWMSModel/Constant/EnumConst/McCommandType.cs
+++ AWMSModel/Constant/EnumConst/McCommandType.cs
@@ -7,14 +7,15 @@ namespace AMSModel.Constant.EnumConst
     public enum McCommandType
     {
         MC_START_HOME = 10,
-
         MC_MOVE_DES = 11,
         MC_MOVE_HOME = 12,
         MC_MOVE_STANBY = 13,
-
         MC_STOMOVE_SOU_DES = 21,
-        MC_STOMOVE_AUTO = 22,
-
+        MC_STOMOVE_RECEIVE = 22,
+        MC_STOMOVE_ISSUE = 23,
         MC_SHUTDOWN_HOME = 32,
+        MC_CHART_HOME = 40,
+        MC_STOMOVE_REJECT = 50
+
     }
 }
--- AWMSModel/Entity/acs_McMaster.cs
+++ AWMSModel/Entity/acs_McMaster.cs
@@ -13,49 +13,91 @@ namespace AMSModel.Entity
         public string LogicalNumber;
 
         public string DK_Set_SouLoc;
-        public int VW_Set_SouLoc;
+        public string DW_Set_SouLoc;
 
-        public string DK_Con_SouLoc;
-        public int VW_Con_SouLoc;
+        public string DK_Set_PalletUnit;
+        public string DW_Set_PalletUnit;
 
-        public string DK_Pre_SouLoc;
-        public int VW_Pre_SouLoc;
+        public string DK_Set_ShtDi;
+        public string DW_Set_ShtDi;
+
+        public string DK_Set_Hand;
+        public string DW_Set_Hand;
 
         public string DK_Set_DesLoc;
-        public int VW_Set_DesLoc;
-
-        public string DK_Con_DesLoc;
-        public int VW_Con_DesLoc;
-
-        public string DK_Pre_DesLoc;
-        public int VW_Pre_DesLoc;
+        public string DW_Set_DesLoc;
 
         public string DK_Set_CurLoc;
-        public int VW_Set_CurLoc;
-
-        public string DK_Con_CurLoc;
-        public int VW_Con_CurLoc;
-
-        public string DK_Pre_CurLoc;
-        public int VW_Pre_CurLoc;
-
-        public string DK_Pre_BarPallet;
-        public int VW_Pre_BarPallet;
-
-        public string DK_Pre_BarProd;
-        public int VW_Pre_BarProd;
-
-        public string DK_Set_Comm;
-        public string DK_Con_Comm;
-        public string DK_Pre_Comm;
+        public string DW_Set_CurLoc;
 
         public string DK_Set_Unit;
+        public string DW_Set_Unit;
+
+        public string DK_Set_PalletID;
+        public string DW_Set_PalletID;
+
+        public string DK_Set_Row;
+        public string DW_Set_Row;
+
+        public string DK_Set_Weigh;
+        public string DW_Set_Weigh;
+
+        public string DK_Set_Comm;
+        public string DW_Set_Comm;
+
+        public string DK_Con_SouLoc;
+        public string DW_Con_SouLoc;
+
+        public string DK_Con_PalletUnit;
+        public string DW_Con_PalletUnit;
+
+        public string DK_Con_ShtDi;
+        public string DW_Con_ShtDi;
+
+        public string DK_Con_Hand;
+        public string DW_Con_Hand;
+
+        public string DK_Con_DesLoc;
+        public string DW_Con_DesLoc;
+
         public string DK_Con_Unit;
-        public string DK_Pre_Unit;
+        public string DW_Con_Unit;
+
+        public string DK_Con_PalletID;
+        public string DW_Con_PalletID;
+
+        public string DK_Con_Weigh;
+        public string DW_Con_Weigh;
+
+        public string DK_Con_Comm;
+        public string DW_Con_Comm;
+
+        public string DK_Con_CurLoc;
+        public string DW_Con_CurLoc;
+
+        public string DK_Pre_SouLoc;
+        public string DW_Pre_SouLoc;
+
+        public string DK_Pre_DesLoc;
+        public string DW_Pre_DesLoc;
+
+        public string DK_Pre_CurLoc;
+        public string DW_Pre_CurLoc;
+
+        public string DK_Pre_Comm;
+        public string DW_Pre_Comm;
+
+        public string DK_Pre_BarPallet;
+        public string DW_Pre_BarPallet;
+
+        public string DK_Pre_BarProd;
+        public string DW_Pre_BarProd;
 
         public string DK_Pre_Status;
-        public string DK_Pre_WeiKG;
+        public string DW_Pre_Status;
 
+        public string DK_Pre_WeiKG;
+        public string DW_Pre_WeiKG;
 
     }
 }
