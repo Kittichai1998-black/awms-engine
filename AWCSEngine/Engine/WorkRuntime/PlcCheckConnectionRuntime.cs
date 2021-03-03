c1bbea429d57bf1b4173247dfbb56ad334c89fe8
Rattana Oudtagosum <memo_love02@hotmail.com>
2021-03-03 13:05:43 +0700

    WCS#8

---
+++ AWCSEngine/Engine/WorkRuntime/PlcCheckConnectionRuntime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AWCSEngine.Engine.WorkRuntime.PlcCheckConnectionRuntime
+{
+    public class PlcCheckConnectionRuntime : BaseWorkRuntime
+    {
+        public PlcCheckConnectionRuntime(string logref) : base(logref)
+        {
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnRun()
+        {
+            int icheck = ADO.WCSPLC.PlcKepwareV6ADO.GetInstant("WH08.PLC#45").GetDevice<int>("CONN_READ");
+            ADO.WCSPLC.PlcKepwareV6ADO.GetInstant("WH08.PLC#45").GetDevice<int>("CONN_WRITE");
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
