831a6f348a0c668b853d09d12020899baa4b0a4a
Anon Phupui <anon_katoon@hotmail.com>
2021-03-29 18:42:15 +0700

    fix return json

--- AWCSWebApp/Startup.cs
+++ AWCSWebApp/Startup.cs
@@ -41,6 +41,10 @@ namespace AWCSWebApp
                     .WithExposedHeaders("x-custom-header");
                 });
             });
+
+            services.AddAuthorization();
+            services.AddControllers();
+            services.AddMvc().AddNewtonsoftJson();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
